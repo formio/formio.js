27d90b1e054da5e15a7f1b0f474b374cfd699df8
Randall Knutson <randall@form.io>
2016-08-09 09:15:35 -0500

    Rebuild.

--- dist/formio.js
+++ dist/formio.js
@@ -2279,7 +2279,12 @@ var dropbox = function(formio) {
         }
 
         xhr.open('POST', formio.formUrl + '/storage/dropbox');
-        var token = localStorage.getItem('formioToken');
+        try {
+          var token = localStorage.getItem('formioToken');
+        }
+        catch (e) {
+          // Swallow error.
+        }
         if (token) {
           xhr.setRequestHeader('x-jwt-token', token);
         }
@@ -2391,7 +2396,12 @@ var s3 = function(formio) {
 
         pre.setRequestHeader('Accept', 'application/json');
         pre.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
-        var token = localStorage.getItem('formioToken');
+        try {
+          var token = localStorage.getItem('formioToken');
+        }
+        catch (e) {
+          // swallow error.
+        }
         if (token) {
           pre.setRequestHeader('x-jwt-token', token);
         }
