4b566f6f41f230d7b2c45033005dcd153b986f13
Travis Tidwell <travis@form.io>
2017-02-08 23:21:45 -0600

    Adding css to embed code.

--- dist/formio.embed.js
+++ dist/formio.embed.js
@@ -10219,9 +10219,12 @@ function _interopRequireDefault(obj) {
 var query = {};
 var scripts = document.getElementsByTagName('script');
 var thisScript = scripts[scripts.length - 1];
+var scriptSrc = thisScript.src.replace(/^([^\?]+).*/, '$1').split('/');
+scriptSrc.pop();
+scriptSrc = scriptSrc.join('/');
 var queryString = thisScript.src.replace(/^[^\?]+\??/, '');
 var id = Math.random().toString(36).substring(7);
-document.write('<div id="' + id + '" style="height:500px;"></div>');
+document.write('<link rel="stylesheet" href="' + scriptSrc + '/formio.form.min.css"><div id="' + id + '" style="height:500px;"></div>');
 queryString.replace(/\?/g, '&').split("&").forEach(function (item) {
   query[item.split("=")[0]] = item.split("=")[1] && decodeURIComponent(item.split("=")[1]);
 });
