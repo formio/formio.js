ee52bad42d9a4c98f5ed3e8554891b9d34244b98
Randall Knutson <randall@form.io>
2017-06-21 12:47:15 -0500

    Add time component.

---
+++ src/components/time/Time.js
@@ -0,0 +1,23 @@
+import moment from 'moment';
+import { TextFieldComponent } from '../textfield/TextField';
+export class TimeComponent extends TextFieldComponent {
+  elementInfo() {
+    let info = super.elementInfo();
+    info.attr.type = 'time';
+    return info;
+  }
+  getValueAt(index) {
+    if (!this.inputs.length || !this.inputs[index]) {
+      return null;
+    }
+    let val = this.inputs[index].value;
+    if (!val) {
+      return null;
+    }
+
+    return moment(val, this.component.format).format('HH:mm:ss');
+  }
+  setValueAt(index, value) {
+    this.inputs[index].value = moment(value, 'HH:mm:ss').format(this.component.format);
+  }
+}
---
+++ src/components/time/fixtures/comp1.js
@@ -0,0 +1,23 @@
+export const component = {
+  "conditional": {
+    "eq": "",
+    "when": null,
+    "show": ""
+  },
+  "tags": [
+
+  ],
+  "type": "time",
+  "persistent": true,
+  "unique": false,
+  "protected": false,
+  "defaultValue": "",
+  "suffix": "",
+  "prefix": "",
+  "placeholder": "Enter a time",
+  "key": "time",
+  "label": "Time",
+  "inputType": "time",
+  "tableView": true,
+  "input": true
+};
