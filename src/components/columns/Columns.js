062ece9d940cb0239f5402f558a15d4887f808af
douglaselee <douglaselee@comcast.net>
2017-07-28 14:20:20 -0400

    Yet more removed unused colWidth from columns

--- src/components/columns/Columns.js
+++ src/components/columns/Columns.js
@@ -5,7 +5,6 @@ export class ColumnsComponent extends FormioComponents {
     return 'row';
   }
   addComponents() {
-    let colWidth = Math.floor(12 / this.component.columns.length);
     _each(this.component.columns, (column) => {
       column.type = 'column';
       this.addComponent(column, this.element, this.data);
