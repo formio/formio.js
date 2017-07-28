1939113d99a19b4d48de1771a78be2612f57b568
douglaselee <douglaselee@comcast.net>
2017-07-28 11:07:38 -0400

    Support custom column width, offset, push and pull

--- src/components/columns/Column.js
+++ src/components/columns/Column.js
@@ -1,6 +1,11 @@
 import { FormioComponents } from '../Components';
 export class ColumnComponent extends FormioComponents {
   get className() {
-    return 'col col-sm-' + this.component.colWidth;
+      var comp   = this.component;
+      var width  = ' col-sm-'        + (comp.width  ? comp.width  : comp.colWidth);
+      var offset = ' col-sm-offset-' + (comp.offset ? comp.offset : 0);  
+      var push   = ' col-sm-push-'   + (comp.push   ? comp.push   : 0);  
+      var pull   = ' col-sm-pull-'   + (comp.pull   ? comp.pull   : 0);  
+      return 'col' + width + offset + push + pull;
   }
 }
