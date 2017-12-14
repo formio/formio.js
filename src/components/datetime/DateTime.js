4f8a5541602d1e79d23cb367dbdfd284547201f1
Yury Rybak <Yury.Rybak@Softensity.com>
2017-12-14 17:09:16 +0300

    ViewAsHtml: Fixed DateTime component.

--- src/components/datetime/DateTime.js
+++ src/components/datetime/DateTime.js
@@ -3,6 +3,7 @@ import Flatpickr from 'flatpickr';
 import _get from 'lodash/get';
 import _each from 'lodash/each';
 import { getDateSetting } from '../../utils';
+import moment from 'moment';
 export class DateTimeComponent extends BaseComponent {
   constructor(component, options, data) {
     super(component, options, data);
@@ -33,37 +34,54 @@ export class DateTimeComponent extends BaseComponent {
   }
 
   /**
-   * Convert the format from the angular-datepicker module.
+   * Convert the format from the angular-datepicker module to flatpickr format.
    * @param format
    * @return {string|XML|*}
    */
-  convertFormat(format) {
+  convertFormatToFlatpickr(format) {
+    return format
+      // Year conversion.
+      .replace(/y/g, 'Y')
+      .replace('YYYY', 'Y')
+      .replace('YY', 'y')
+
+      // Month conversion.
+      .replace('MMMM', 'F')
+      .replace(/M/g, 'n')
+      .replace('nnn', 'M')
+      .replace('nn', 'm')
+
+      // Day in month.
+      .replace(/d/g, 'j')
+      .replace('jj', 'd')
+
+      // Day in week.
+      .replace('EEEE', 'l')
+      .replace('EEE', 'D')
+
+      // Hours, minutes, seconds
+      .replace('HH', 'H')
+      .replace('hh', 'h')
+      .replace('mm', 'i')
+      .replace('ss', 'S')
+      .replace(/a/g, 'K');
+  }
+
+  /**
+   * Convert the format from the angular-datepicker module to moment format.
+   * @param format
+   * @return {string}
+   */
+  convertFormatToMoment(format) {
+    return format
     // Year conversion.
-    format = format.replace(/y/g, 'Y');
-    format = format.replace('YYYY', 'Y');
-    format = format.replace('YY', 'y');
-
-    // Month conversion.
-    format = format.replace('MMMM', 'F');
-    format = format.replace(/M/g, 'n');
-    format = format.replace('nnn', 'M');
-    format = format.replace('nn', 'm');
-
+    .replace(/y/g, 'Y')
     // Day in month.
-    format = format.replace(/d/g, 'j');
-    format = format.replace('jj', 'd');
-
+    .replace(/d/g, 'D')
     // Day in week.
-    format = format.replace('EEEE', 'l');
-    format = format.replace('EEE', 'D');
-
-    // Hours, minutes, seconds
-    format = format.replace('HH', 'H');
-    format = format.replace('hh', 'h');
-    format = format.replace('mm', 'i');
-    format = format.replace('ss', 'S');
-    format = format.replace(/a/g, 'K');
-    return format;
+    .replace(/E/g, 'd')
+    // AM/PM marker
+    .replace(/a/g, 'A');
   }
 
   get config() {
@@ -74,7 +92,7 @@ export class DateTimeComponent extends BaseComponent {
       mode: this.component.multiple ? 'multiple' : 'single',
       enableTime: _get(this.component, 'enableTime', true),
       noCalendar: !_get(this.component, 'enableDate', true),
-      altFormat: this.convertFormat(_get(this.component, 'format', '')),
+      altFormat: this.convertFormatToFlatpickr(_get(this.component, 'format', '')),
       dateFormat: 'U',
       defaultDate: this.defaultDate,
       hourIncrement: _get(this.component, 'timePicker.hourStep', 1),
@@ -170,6 +188,11 @@ export class DateTimeComponent extends BaseComponent {
     }
 
     return dates[0].toISOString();
+  }
+
+  get viewOnlyValue() {
+    const value = this.getValue();
+    return value ? moment(value).format(this.convertFormatToMoment(_get(this.component, 'format', ''))) : null;
   }
 
   setValueAt(index, value) {
