47b3b27eef1ac0f154d9b21edb77e3febcc952b7
Yury Rybak <yuryrybak97@gmail.com>
2018-02-07 17:31:58 +0300

    Improvement: removed babel-plugin-lodash.

--- gulp/babel.js
+++ gulp/babel.js
@@ -1,11 +1,7 @@
 module.exports = function(gulp, plugins) {
   return function () {
     return gulp.src('./src/**/*.js')
-      .pipe(plugins.if('formio.js', plugins.babel({
-        plugins: [
-          'lodash'
-        ]
-      }), plugins.babel()))
+      .pipe(plugins.babel())
       .pipe(gulp.dest('build/'));
   };
 };
