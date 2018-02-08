4195fdb22f8ebd063e7c188be806f0e3375cdb13
James R. Maxfield <Vercjames@gmail.com>
2018-02-08 14:31:53 -0600

    Added WYSIWYG Spellcheck support for quill

--- src/components/textarea/TextArea.js
+++ src/components/textarea/TextArea.js
@@ -55,6 +55,7 @@ export class TextAreaComponent extends TextFieldComponent {
     this.quillReady = BaseComponent.requireLibrary('quill', 'Quill', 'https://cdn.quilljs.com/1.3.3/quill.min.js', true)
       .then(() => {
         this.quill = new Quill(this.input, this.component.wysiwyg);
+        this.quill.root.spellcheck = this.component.spellcheck;
 
         /** This block of code adds the [source] capabilities.  See https://codepen.io/anon/pen/ZyEjrQ **/
         var txtArea = document.createElement('textarea');
