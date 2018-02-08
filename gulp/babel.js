10e9dc045221247580132bae31d58f7448a61615
Yury Rybak <Yury.Rybak@Softensity.com>
2018-02-08 18:12:32 +0300

    Improvement: fixed build.

--- gulp/babel.js
+++ gulp/babel.js
@@ -1,6 +1,6 @@
 module.exports = function(gulp, plugins) {
   return function () {
-    return gulp.src((['./src/**/*.js', '!./src/**/*.spec.js'])
+    return gulp.src(['./src/**/*.js', '!./src/**/*.spec.js'])
       .pipe(plugins.babel())
       .pipe(gulp.dest('lib/'));
   };
