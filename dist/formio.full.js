1f362dcea075bf36e5c74be5b5269688ba5fce47
Travis Tidwell <travis@form.io>
2018-02-15 10:36:29 -0600

    Updated build.

--- dist/formio.full.js
+++ dist/formio.full.js
@@ -1932,19 +1932,26 @@ var BaseComponent = function () {
   }, {
     key: 'setupValueElement',
     value: function setupValueElement(element) {
-      var value = this.value;
+      var value = this.getValue();
       value = this.isEmpty(value) ? this.defaultViewOnlyValue : this.getView(value);
-      element.appendChild(this.text(value));
+      element.innerHTML = value;
     }
   }, {
     key: 'getView',
     value: function getView(value) {
-      return _lodash2.default.toString(value);
+      if (Array.isArray(value)) {
+        return value.join(', ');
+      }
+
+      return value.toString();
     }
   }, {
     key: 'updateViewOnlyValue',
     value: function updateViewOnlyValue() {
-      this.empty(this.valueElement);
+      if (!this.valueElement) {
+        return;
+      }
+
       this.setupValueElement(this.valueElement);
     }
   }, {
@@ -9870,9 +9877,9 @@ var SelectBoxesComponent = exports.SelectBoxesComponent = function (_RadioCompon
   }, {
     key: 'getView',
     value: function getView(value) {
-      return _lodash2.default.flow(_lodash2.default.filter(function (v) {
+      return (0, _lodash2.default)(this.component.values || []).filter(function (v) {
         return value[v.value];
-      }), _lodash2.default.map('label'), _lodash2.default.join(', '))(this.component.values || []);
+      }).map('label').join(', ');
     }
   }]);
 
@@ -10208,60 +10215,64 @@ var SurveyComponent = exports.SurveyComponent = function (_BaseComponent) {
     value: function build() {
       var _this2 = this;
 
-      this.createElement();
-      var labelAtTheBottom = this.component.labelPosition === 'bottom';
-      if (!labelAtTheBottom) {
-        this.createLabel(this.element);
-      }
-      this.table = this.ce('table', {
-        class: 'table table-striped table-bordered'
-      });
-      this.setInputStyles(this.table);
-
-      // Build header.
-      var thead = this.ce('thead');
-      var thr = this.ce('tr');
-      thr.appendChild(this.ce('td'));
-      _lodash2.default.each(this.component.values, function (value) {
-        var th = _this2.ce('th', {
-          style: 'text-align: center;'
+      if (this.viewOnly) {
+        this.viewOnlyBuild();
+      } else {
+        this.createElement();
+        var labelAtTheBottom = this.component.labelPosition === 'bottom';
+        if (!labelAtTheBottom) {
+          this.createLabel(this.element);
+        }
+        this.table = this.ce('table', {
+          class: 'table table-striped table-bordered'
         });
-        th.appendChild(_this2.text(value.label));
-        thr.appendChild(th);
-      });
-      thead.appendChild(thr);
-      this.table.appendChild(thead);
-      // Build the body.
-      var tbody = this.ce('tbody');
-      _lodash2.default.each(this.component.questions, function (question) {
-        var tr = _this2.ce('tr');
-        var td = _this2.ce('td');
-        td.appendChild(_this2.text(question.label));
-        tr.appendChild(td);
-        _lodash2.default.each(_this2.component.values, function (value) {
-          var td = _this2.ce('td', {
+        this.setInputStyles(this.table);
+
+        // Build header.
+        var thead = this.ce('thead');
+        var thr = this.ce('tr');
+        thr.appendChild(this.ce('td'));
+        _lodash2.default.each(this.component.values, function (value) {
+          var th = _this2.ce('th', {
             style: 'text-align: center;'
           });
-          var input = _this2.ce('input', {
-            type: 'radio',
-            name: 'data[' + _this2.component.key + '][' + question.value + ']',
-            value: value.value,
-            id: _this2.id + '-' + question.value + '-' + value.value
-          });
-          _this2.addInput(input, td);
-          tr.appendChild(td);
+          th.appendChild(_this2.text(value.label));
+          thr.appendChild(th);
         });
-        tbody.appendChild(tr);
-      });
-      this.table.appendChild(tbody);
-      this.element.appendChild(this.table);
-      if (labelAtTheBottom) {
-        this.createLabel(this.element);
-      }
-      this.createDescription(this.element);
-      this.restoreValue();
-      if (this.shouldDisable) {
-        this.disabled = true;
+        thead.appendChild(thr);
+        this.table.appendChild(thead);
+        // Build the body.
+        var tbody = this.ce('tbody');
+        _lodash2.default.each(this.component.questions, function (question) {
+          var tr = _this2.ce('tr');
+          var td = _this2.ce('td');
+          td.appendChild(_this2.text(question.label));
+          tr.appendChild(td);
+          _lodash2.default.each(_this2.component.values, function (value) {
+            var td = _this2.ce('td', {
+              style: 'text-align: center;'
+            });
+            var input = _this2.ce('input', {
+              type: 'radio',
+              name: 'data[' + _this2.component.key + '][' + question.value + ']',
+              value: value.value,
+              id: _this2.id + '-' + question.value + '-' + value.value
+            });
+            _this2.addInput(input, td);
+            tr.appendChild(td);
+          });
+          tbody.appendChild(tr);
+        });
+        this.table.appendChild(tbody);
+        this.element.appendChild(this.table);
+        if (labelAtTheBottom) {
+          this.createLabel(this.element);
+        }
+        this.createDescription(this.element);
+        this.restoreValue();
+        if (this.shouldDisable) {
+          this.disabled = true;
+        }
       }
     }
   }, {
@@ -10302,6 +10313,37 @@ var SurveyComponent = exports.SurveyComponent = function (_BaseComponent) {
         });
       });
       return value;
+    }
+  }, {
+    key: 'getView',
+    value: function getView(value) {
+      var _this5 = this;
+
+      var table = this.ce('table', {
+        class: 'table table-striped table-bordered table-condensed'
+      });
+      var tbody = this.ce('tbody');
+
+      _lodash2.default.each(value, function (value, question) {
+        var row = _this5.ce('tr');
+
+        var questionCell = _this5.ce('th');
+        var valueCell = _this5.ce('td');
+
+        var questionText = _lodash2.default.find(_this5.component.questions, ['value', question]).label;
+        var valueText = _lodash2.default.find(_this5.component.values, ['value', value]).label;
+
+        questionCell.appendChild(_this5.text(questionText));
+        valueCell.appendChild(_this5.text(valueText));
+
+        row.appendChild(questionCell);
+        row.appendChild(valueCell);
+
+        tbody.appendChild(row);
+      });
+
+      table.appendChild(tbody);
+      return table.outerHTML;
     }
   }]);
 
