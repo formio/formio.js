841558d01630a69338e57acfefd0f9f42459e580
Randall Knutson <randall@form.io>
2018-07-19 11:58:34 -0500

    Make hidden components actually hide.

--- src/templates/semantic/component.js
+++ src/templates/semantic/component.js
@@ -1,8 +1,10 @@
 export default {
   form: `
 <div id="{{id}}" class="field {{classes}}"{% if (styles) { %} styles="{{styles}}"{% } %} ref="component">
+  {% if (visible) { %}
   {{children}}
   <div ref="messageContainer"></div>
+  {% } %}
 </div>
 `,
 };
--- src/templates/semantic/iconClass.js
+++ src/templates/semantic/iconClass.js
@@ -3,13 +3,12 @@ export default (iconset, name, spinning) => {
     'question-sign': 'question circle',
     'remove-circle': 'trash alternate outline',
     'new-window': 'external alternate',
-    'clock-o': 'clock outline',
-    'dot-circle-o': 'dot circle outline',
     'files-o': 'file outline',
   };
   if (icons.hasOwnProperty(name)) {
     name = icons[name];
   }
-  name = name.replace('-', ' ');
+  name = name.replace(/-/g, ' ');
+  name = name.replace(/ o$/, ' outline');
   return spinning ? `icon ${name} loading` : `icon ${name}`;
 };
