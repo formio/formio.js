6310be365b384d9e7118e2e2f13cfa3bf07612bf
Travis Tidwell <travis@form.io>
2018-08-20 13:05:23 -0500

    Updated embed script.

--- src/formio.embed.js
+++ src/formio.embed.js
@@ -2,7 +2,7 @@ const scripts = document.getElementsByTagName('script');
 let thisScript = scripts[(scripts.length - 1)];
 let foundScript = false;
 if (!thisScript.src || thisScript.src.indexOf('formio.embed') === -1) {
-  // Manually search for the script...
+  // Fallback mode: Manually search for the script...
   for (var i in scripts) {
     if (
       scripts[i].src &&
@@ -18,53 +18,54 @@ else {
   foundScript = true;
 }
 
-// Show an error if the script cannot be found.
-if (!foundScript) {
-  return document.write('<span>Could not locate form render script.</span>');
-}
-
-const Form = require('./formio.form').Form;
-const query = {};
-let scriptSrc = thisScript.src.replace(/^([^?]+).*/, '$1').split('/');
-scriptSrc.pop();
-scriptSrc = scriptSrc.join('/');
-const queryString = thisScript.src.replace(/^[^?]+\??/,'');
-queryString.replace(/\?/g, '&').split('&').forEach((item) => {
-  query[item.split('=')[0]] = item.split('=')[1] && decodeURIComponent(item.split('=')[1]);
-});
-query.styles = query.styles || (`${scriptSrc}/formio.full.min.css`);
-Form.embed(query).then((instance) => {
-  instance.on('submit', (submission) => {
-    let returnUrl = query.return || query.redirect;
-
-    // Allow form based configuration for return url.
-    if (
-      !returnUrl &&
-      (
-        instance._form &&
-        instance._form.settings &&
-        (
-          instance._form.settings.returnUrl ||
-          instance._form.settings.redirect
-        )
-      )
-    ) {
-      returnUrl = instance._form.settings.returnUrl || instance._form.settings.redirect;
-    }
-
-    if (returnUrl) {
-      const formSrc = instance.formio ? instance.formio.formUrl : '';
-      const hasQuery = !!returnUrl.match(/\?/);
-      const isOrigin = returnUrl.indexOf(location.origin) === 0;
-      returnUrl += hasQuery ? '&' : '?';
-      returnUrl += `sub=${submission._id}`;
-      if (!isOrigin && formSrc) {
-        returnUrl += `&form=${encodeURIComponent(formSrc)}`;
-      }
-      window.location.href = returnUrl;
-      if (isOrigin) {
-        window.location.reload();
-      }
-    }
+if (foundScript) {
+  const Form = require('./formio.form').Form;
+  const query = {};
+  let scriptSrc = thisScript.src.replace(/^([^?]+).*/, '$1').split('/');
+  scriptSrc.pop();
+  scriptSrc = scriptSrc.join('/');
+  const queryString = thisScript.src.replace(/^[^?]+\??/,'');
+  queryString.replace(/\?/g, '&').split('&').forEach((item) => {
+    query[item.split('=')[0]] = item.split('=')[1] && decodeURIComponent(item.split('=')[1]);
   });
-});
+  query.styles = query.styles || (`${scriptSrc}/formio.full.min.css`);
+  Form.embed(query).then((instance) => {
+    instance.on('submit', (submission) => {
+      let returnUrl = query.return || query.redirect;
+
+      // Allow form based configuration for return url.
+      if (
+        !returnUrl &&
+        (
+          instance._form &&
+          instance._form.settings &&
+          (
+            instance._form.settings.returnUrl ||
+            instance._form.settings.redirect
+          )
+        )
+      ) {
+        returnUrl = instance._form.settings.returnUrl || instance._form.settings.redirect;
+      }
+
+      if (returnUrl) {
+        const formSrc = instance.formio ? instance.formio.formUrl : '';
+        const hasQuery = !!returnUrl.match(/\?/);
+        const isOrigin = returnUrl.indexOf(location.origin) === 0;
+        returnUrl += hasQuery ? '&' : '?';
+        returnUrl += `sub=${submission._id}`;
+        if (!isOrigin && formSrc) {
+          returnUrl += `&form=${encodeURIComponent(formSrc)}`;
+        }
+        window.location.href = returnUrl;
+        if (isOrigin) {
+          window.location.reload();
+        }
+      }
+    });
+  });
+}
+else {
+  // Show an error if the script cannot be found.
+  document.write('<span>Could not locate form render script.</span>');
+}
