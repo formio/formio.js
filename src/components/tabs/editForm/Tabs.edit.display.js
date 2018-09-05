1e59fb4f157638a5bcb5600397ea8f090c7ca9c6
= <=>
2018-09-05 15:59:48 +0200

    Tabs: added the ability to render each tab's content on first load.

--- src/components/tabs/editForm/Tabs.edit.display.js
+++ src/components/tabs/editForm/Tabs.edit.display.js
@@ -20,5 +20,13 @@ export default [
         calculateValue: { _camelCase: [{ var: 'row.label' }] }
       }
     ]
+  },
+  {
+    type: 'checkbox',
+    input: true,
+    label: 'Render all tabs content',
+    key: 'renderAllTabsContent',
+    tooltip: 'When set, each tab\'s content will be rendered on the first load. By default, only the first tab has its content rendered',
+    weight: 11
   }
 ];
