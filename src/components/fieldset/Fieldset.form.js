4552d1667fe39ed725de4d9f7f59365b75e81472
Travis Tidwell <travis@form.io>
2018-09-09 10:31:40 -0500

    Adding legend settings to the fieldset and hide the label field.

--- src/components/fieldset/Fieldset.form.js
+++ src/components/fieldset/Fieldset.form.js
@@ -1,4 +1,10 @@
 import nestedComponentForm from '../nested/NestedComponent.form';
+import FieldSetEditDisplay from './editForm/Fieldset.edit.display';
 export default function(...extend) {
-  return nestedComponentForm(...extend);
+  return nestedComponentForm([
+    {
+      key: 'display',
+      components: FieldSetEditDisplay
+    }
+  ], ...extend);
 }
