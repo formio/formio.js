746c5a0d89a6529020357e3ff53f17f6ec7cf080
iobuhov <vyusfire@gmail.com>
2018-09-13 13:05:48 +0300

    FOR-672: Add uiquify step to copyComponent method.

--- src/WebformBuilder.js
+++ src/WebformBuilder.js
@@ -654,6 +654,7 @@ export default class WebformBuilder extends Component {
     }
     this.addClass(this.refs.form, 'builder-paste-mode');
     const copy = _.cloneDeep(component.schema);
+    BuilderUtils.uniquify(this._form, copy);
     window.sessionStorage.setItem('formio.clipboard', JSON.stringify(copy));
   }
 
