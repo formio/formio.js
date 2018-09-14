d10c242a99581d61094cbf7b42e3a235f79518c8
Travis Tidwell <travis@form.io>
2018-09-14 11:21:25 -0500

    FOR-1363: Fixed issue with the validation checks occurring twice from button component.

--- src/components/button/Button.js
+++ src/components/button/Button.js
@@ -143,7 +143,7 @@ export default class ButtonComponent extends Field {
       });
       this.on('change', (value) => {
         this.loading = false;
-        const isValid = this.root.isValid(value.data, true);
+        const isValid = value.hasOwnProperty('isValid') ? value.isValid : this.root.isValid(value.data, true);
         this.disabled = this.options.readOnly || (this.component.disableOnInvalid && !isValid);
         this.removeClass(this.refs.button, 'btn-success submit-success');
         this.removeClass(this.refs.button, 'btn-danger submit-fail');
