9ef0b0b41d5703b5162ea26396c3b7f59bb34e64
Travis Tidwell <travis@form.io>
2018-10-03 14:13:52 -0500

    Changing the positions of the calculated value settings.

--- src/components/base/editForm/Base.edit.data.js
+++ src/components/base/editForm/Base.edit.data.js
@@ -10,22 +10,6 @@ export default [
     tooltip: 'The will be the value for this field, before user interaction. Having a default value will override the placeholder text.',
     input: true
   },
-  EditFormUtils.javaScriptValue('Custom Default Value', 'customDefaultValue', 'customDefaultValue', 110,
-    '<p><h4>Example:</h4><pre>value = data.firstName + " " + data.lastName;</pre></p>',
-    '<p><h4>Example:</h4><pre>{"cat": [{"var": "data.firstName"}, " ", {"var": "data.lastName"}]}</pre>'
-  ),
-  EditFormUtils.javaScriptValue('Calculated Value', 'calculateValue', 'calculateValue', 120,
-    '<p><h4>Example:</h4><pre>value = data.a + data.b + data.c;</pre></p>',
-    '<p><h4>Example:</h4><pre>{"sum": [{"var": "data.a"}, {"var": "data.b"}, {"var": "data.c"}]}</pre><p><a target="_blank" href="http://formio.github.io/formio.js/app/examples/calculated.html">Click here for an example</a></p>'
-  ),
-  {
-    type: 'checkbox',
-    input: true,
-    weight: 109,
-    key: 'allowCalculateOverride',
-    label: 'Allow Manual Override of Calculated Value',
-    tooltip: 'When checked, this will allow the user to manually override the calculated value.'
-  },
   {
     type: 'select',
     input: true,
@@ -55,6 +39,22 @@ export default [
     key: 'clearOnRefresh',
     label: 'Clear Value On Refresh',
     tooltip: 'When the Refresh On field is changed, clear this components value.'
+  },
+  EditFormUtils.javaScriptValue('Custom Default Value', 'customDefaultValue', 'customDefaultValue', 120,
+    '<p><h4>Example:</h4><pre>value = data.firstName + " " + data.lastName;</pre></p>',
+    '<p><h4>Example:</h4><pre>{"cat": [{"var": "data.firstName"}, " ", {"var": "data.lastName"}]}</pre>'
+  ),
+  EditFormUtils.javaScriptValue('Calculated Value', 'calculateValue', 'calculateValue', 130,
+    '<p><h4>Example:</h4><pre>value = data.a + data.b + data.c;</pre></p>',
+    '<p><h4>Example:</h4><pre>{"sum": [{"var": "data.a"}, {"var": "data.b"}, {"var": "data.c"}]}</pre><p><a target="_blank" href="http://formio.github.io/formio.js/app/examples/calculated.html">Click here for an example</a></p>'
+  ),
+  {
+    type: 'checkbox',
+    input: true,
+    weight: 131,
+    key: 'allowCalculateOverride',
+    label: 'Allow Manual Override of Calculated Value',
+    tooltip: 'When checked, this will allow the user to manually override the calculated value.'
   },
   {
     weight: 400,
