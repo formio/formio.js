48a38059283fc6d1e6bf96ada5c620f2d7426972
Yury Rybak <yury.rybak@softensity.com>
2018-10-22 17:26:50 +0300

    FOR-1757: Improved content property.

--- src/components/html/editForm/HTML.edit.logic.js
+++ src/components/html/editForm/HTML.edit.logic.js
@@ -14,66 +14,80 @@ export default [
                     {
                       label: 'Hidden',
                       value: 'hidden',
-                      type: 'boolean'
+                      type: 'boolean',
                     },
                     {
                       label: 'Required',
                       value: 'validate.required',
-                      type: 'boolean'
+                      type: 'boolean',
                     },
                     {
                       label: 'Disabled',
                       value: 'disabled',
-                      type: 'boolean'
+                      type: 'boolean',
                     },
                     {
                       label: 'Label',
                       value: 'label',
-                      type: 'string'
+                      type: 'string',
                     },
                     {
                       label: 'Title',
                       value: 'title',
-                      type: 'string'
+                      type: 'string',
                     },
                     {
                       label: 'Tooltip',
                       value: 'tooltip',
-                      type: 'string'
+                      type: 'string',
                     },
                     {
                       label: 'Decription',
                       value: 'description',
-                      type: 'string'
+                      type: 'string',
                     },
                     {
                       label: 'Paceholder',
                       value: 'placeholder',
-                      type: 'string'
+                      type: 'string',
                     },
                     {
                       label: 'CSS Class',
                       value: 'className',
-                      type: 'string'
+                      type: 'string',
                     },
                     {
                       label: 'Container Custom Class',
                       value: 'customClass',
-                      type: 'string'
+                      type: 'string',
                     },
                     {
                       label: 'Content',
                       value: 'content',
-                      type: 'string'
-                    }
+                      type: 'string',
+                      component: 'content',
+                    },
                   ],
                 },
                 key: 'property',
-              }
+              },
+              {
+                type: 'textarea',
+                editor: 'ace',
+                rows: 10,
+                as: 'html',
+                label: 'Content',
+                tooltip: 'The content of this HTML element.',
+                defaultValue: '<div class="well">Content</div>',
+                key: 'content',
+                weight: 30,
+                input: true,
+                customConditional: 'show = row.type === "property" && row.hasOwnProperty("property") && row.property.type === "string" && row.property.component === "content";',
+              },
             ],
-          }
+          },
         ],
-      }
-    ]
-  }
+      },
+    ],
+  },
 ];
