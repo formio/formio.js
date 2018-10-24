17b9c192e41b7b5e32324f8f2593a55e80db3e57
rakeshBalasubramani <rakesh.balasubramani@gmail.com>
2018-10-24 18:15:24 -0500

    Component

--- src/components/_classes/component/Component.unit.js
+++ src/components/_classes/component/Component.unit.js
@@ -29,14 +29,91 @@ describe('Base Component Unit Tests', () => {
     assert.equal(component.key,'testComponent');
     assert.equal(component.defaultValue,'This is the default value.');
   });
-  it('Setting Default value',() => {
+  it('Setting placeholder',() => {
     const component = new Component({
       label:'Test Component',
       key: 'testComponent',
-      defaultValue: 'This is the default value.'
+      placeholder:'This is the placeholder.'
     });
     assert.equal(component.label,'Test Component');
     assert.equal(component.key,'testComponent');
-    assert.equal(component.defaultValue,'This is the default value.');
+    assert.equal(component.component.placeholder,'This is the placeholder.');
+  });
+  it('Setting prefix',() => {
+    const component = new Component({
+      label:'Test Component',
+      key: 'testComponent',
+      prefix:'Prefix'
+    });
+    assert.equal(component.label,'Test Component');
+    assert.equal(component.key,'testComponent');
+    assert.equal(component.component.prefix,'Prefix');
+  });
+  it('Setting suffix',() => {
+    const component = new Component({
+      label:'Test Component',
+      key: 'testComponent',
+      suffix:'Suffix'
+    });
+    assert.equal(component.label,'Test Component');
+    assert.equal(component.key,'testComponent');
+    assert.equal(component.component.suffix,'Suffix');
+  });
+  it('Setting suffix and prefix',() => {
+    const component = new Component({
+      label:'Test Component',
+      key: 'testComponent',
+      suffix: 'Suffix',
+      prefix: 'Prefix'
+    });
+    assert.equal(component.label,'Test Component');
+    assert.equal(component.key,'testComponent');
+    assert.equal(component.component.suffix,'Suffix');
+    assert.equal(component.component.prefix,'Prefix');
+  });
+  it('Setting component to be required',() => {
+    const component = new Component({
+      label:'Test Component',
+      key: 'testComponent',
+      validate: {
+        required: true
+      }
+    });
+    assert.equal(component.label,'Test Component');
+    assert.equal(component.key,'testComponent');
+    assert.equal(component.component.validate.required,true);
+  });
+  it('Fulfilling required field with default value.',() => {
+    const component = new Component({
+      label:'Test Component',
+      key: 'testComponent',
+      validate: {
+        required: true
+      }
+    });
+    console.log(component.errors);
+    assert.equal(component.label,'Test Component');
+    assert.equal(component.key,'testComponent');
+    assert.equal(component.error,'');
+  });
+  it('Setting custom class',() => {
+    const component = new Component({
+      label:'Test Component',
+      key: 'testComponent',
+      customClass: 'my-custom-class'
+    });
+    assert.equal(component.label,'Test Component');
+    assert.equal(component.key,'testComponent');
+    assert.equal(component.component.customClass,'my-custom-class');
+  });
+  it('Setting multiple values',() => {
+    const component = new Component({
+      label:'Test Component',
+      key: 'testComponent',
+      multiple: true
+    });
+    assert.equal(component.label,'Test Component');
+    assert.equal(component.key,'testComponent');
+    assert.equal(component.component.multiple,true);
   });
 });
