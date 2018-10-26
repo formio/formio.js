9367f4a30db3a575162c98a1c3b0c97f9745032b
Yury Rybak <yury.rybak@softensity.com>
2018-10-26 19:00:26 +0300

    FIR-1759: Added coalescing operator support.

--- babel.config.js
+++ babel.config.js
@@ -11,6 +11,7 @@ module.exports = {
     ],
   ],
   plugins: [
+    '@babel/plugin-proposal-nullish-coalescing-operator',
     '@babel/plugin-proposal-export-default-from',
     '@babel/plugin-proposal-optional-chaining',
   ],
