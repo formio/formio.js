cb48992f6e8dc063422cd78b99c399b0fe28747c
Ilya Obukhov <iobuhov.mail@gmail.com>
2018-12-14 11:53:45 +0300

    Fix bug with editForm Data tab for container components
    Now for Container, DataGrid, DataMap and EditGrid components
    defaultValue filed in editForm would be ignored.

--- src/components/container/Container.form.js
+++ src/components/container/Container.form.js
@@ -1,4 +1,10 @@
 import baseEditForm from '../_classes/component/Component.form';
 export default function(...extend) {
-  return baseEditForm(...extend);
+  return baseEditForm([{
+    key: 'data',
+    components: [{
+      key: 'defaultValue',
+      ignore: true
+    }]
+  }], ...extend);
 }
