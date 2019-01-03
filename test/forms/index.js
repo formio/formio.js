f444222b39babb4ab19c384f22d5d12f625e04eb
Ilya Obuhov <vyusfire@gmail.com>
2019-01-03 18:57:08 +0300

    Fix bug with nested form lang change

--- test/forms/index.js
+++ test/forms/index.js
@@ -5,6 +5,7 @@ import FieldLogic from './fieldLogic';
 import ClearOnHide from './clearOnHide';
 import ClearOnHide2 from './clearOnHide2';
 import SubmissionSetter from './submissionSetter';
+import NestedFormTests from './nested-form-tests';
 
 export default [
   Simple,
@@ -13,5 +14,6 @@ export default [
   Calculated,
   FieldLogic,
   ClearOnHide,
-  ClearOnHide2
+  ClearOnHide2,
+  NestedFormTests,
 ];
