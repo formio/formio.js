f2b1b951b90ad816ed4cf7d5155da6880b401f52
Will Gay <william@form.io>
2019-01-10 17:07:48 -0600

    Update Container.unit.js

--- src/components/container/Container.unit.js
+++ src/components/container/Container.unit.js
@@ -1,7 +1,8 @@
 import ContainerComponent from './Container';
 import assert from 'assert';
+import ColumnsComponent from '../columns/Columns';
 describe('Container Unit Tests', () => {
-  it('Should create a new Columns component', () => {
+  it('Should create a new Container component', () => {
     const container = new ContainerComponent({
       label: 'Container',
       key: 'container',
@@ -10,4 +11,45 @@ describe('Container Unit Tests', () => {
 
     assert.equal(container.component.key, 'container');
   });
+  it('Should be able to change the Container label',() => {
+    const container = new ContainerComponent({
+      label: 'New Label',
+      key: 'container',
+      type: 'container'
+    });
+  });
+  it('Should be able to add component to the Container',() => {
+    const container = new ColumnsComponent({
+      container: [
+        { components: [
+            {
+              'label': 'Text Field',
+              'allowMultipleMasks': false,
+              'showWordCount': false,
+              'showCharCount': false,
+              'tableView': true,
+              'alwaysEnabled': false,
+              'type': 'textfield',
+              'input': true,
+              'key': 'textField',
+              'widget': {
+                'type': ''
+              }
+            }
+          ],
+          'width': 6,
+          'offset': 0,
+          'push': 0,
+          'pull': 0,
+          'type': 'column',
+          'hideOnChildrenHidden': false,
+          'input': true,
+          'key': '',
+          'tableView': true,
+          'label': ''
+        }
+      ]
+  });
 });
+  });
+
