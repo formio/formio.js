9c9a2ae9660ec279d550d6f3deb8a38eadf25473
Ilya Obuhov <vyusfire@gmail.com>
2019-01-11 17:05:52 +0300

    Add Panel breadcrumb settings
    - breadcrumb settings exposed to form build
    - history api get removed from wizard (previous button now
      go only backward)

--- src/Wizard.js
+++ src/Wizard.js
@@ -2,7 +2,7 @@ import _ from 'lodash';
 import Webform from './Webform';
 import Component from './components/_classes/component/Component';
 import Formio from './Formio';
-import { checkCondition } from './utils/utils';
+import { checkCondition, firstNonNil } from './utils/utils';
 
 export default class Wizard extends Webform {
   /**
@@ -19,7 +19,6 @@ export default class Wizard extends Webform {
     this.globalComponents = [];
     this.components = [];
     this.page = 0;
-    this.history = [];
     this._seenPages = [0];
   }
 
@@ -72,7 +71,7 @@ export default class Wizard extends Webform {
       clickable: true
     });
 
-    this.currentPage = 0;
+    this.page = 0;
     return super.init();
   }
 
@@ -103,8 +102,8 @@ export default class Wizard extends Webform {
       wizardKey: this.wizardKey,
       panels: this.panels,
       buttons: this.buttons,
-      currentPage: this.currentPage,
-      components: this.renderComponents([...this.globalComponents, ...this.pages[this.currentPage]]),
+      currentPage: this.page,
+      components: this.renderComponents([...this.globalComponents, ...this.pages[this.page]]),
     }, (this.options.attachMode === 'builder') ? 'builder' : 'form');
   }
 
@@ -119,7 +118,7 @@ export default class Wizard extends Webform {
       [`${this.wizardKey}-link`]: 'multiple',
     });
 
-    const promises = this.attachComponents(this.refs[this.wizardKey], [...this.globalComponents, ...this.pages[this.currentPage]]);
+    const promises = this.attachComponents(this.refs[this.wizardKey], [...this.globalComponents, ...this.pages[this.page]]);
 
     [
       { name: 'cancel',    method: 'cancel' },
@@ -171,7 +170,7 @@ export default class Wizard extends Webform {
           const page = [];
           _.each(item.components, (comp) => {
             const component = this.createComponent(comp, pageOptions);
-            component.page = this.currentPage;
+            component.page = this.page;
             page.push(component);
           });
           this.pages.push(page);
@@ -185,11 +184,11 @@ export default class Wizard extends Webform {
   }
 
   setPage(num) {
-    if (num === this.currentPage) {
+    if (num === this.page) {
       return;
     }
     if (!this.wizard.full && num >= 0 && num < this.pages.length) {
-      this.currentPage = num;
+      this.page = num;
       if (!this._seenPages.includes(num)) {
         this._seenPages = this._seenPages.concat(num);
       }
@@ -234,12 +233,7 @@ export default class Wizard extends Webform {
   }
 
   getPreviousPage() {
-    const prev = this.history.pop();
-    if (typeof prev !== 'undefined') {
-      return prev;
-    }
-
-    return this.currentPage - 1;
+    return Math.max(this.page - 1, 0);
   }
 
   beforeSubmit() {
@@ -252,9 +246,8 @@ export default class Wizard extends Webform {
   nextPage() {
     // Read-only forms should not worry about validation before going to next page, nor should they submit.
     if (this.options.readOnly) {
-      this.history.push(this.currentPage);
-      return this.setPage(this.getNextPage(this.submission.data, this.currentPage)).then(() => {
-        this.emit('nextPage', { page: this.currentPage, submission: this.submission });
+      return this.setPage(this.getNextPage(this.submission.data, this.page)).then(() => {
+        this.emit('nextPage', { page: this.page, submission: this.submission });
       });
     }
 
@@ -264,9 +257,8 @@ export default class Wizard extends Webform {
         noValidate: true
       });
       return this.beforeNext().then(() => {
-        this.history.push(this.currentPage);
-        return this.setPage(this.getNextPage(this.submission.data, this.currentPage)).then(() => {
-          this.emit('nextPage', { page: this.currentPage, submission: this.submission });
+        return this.setPage(this.getNextPage(this.submission.data, this.page)).then(() => {
+          this.emit('nextPage', { page: this.page, submission: this.submission });
         });
       });
     }
@@ -278,13 +270,12 @@ export default class Wizard extends Webform {
   prevPage() {
     const prevPage = this.getPreviousPage();
     return this.setPage(prevPage).then(() => {
-      this.emit('prevPage', { page: this.currentPage, submission: this.submission });
+      this.emit('prevPage', { page: this.page, submission: this.submission });
     });
   }
 
   cancel(noconfirm) {
     if (super.cancel(noconfirm)) {
-      this.history = [];
       return this.setPage(0);
     }
     else {
@@ -304,7 +295,7 @@ export default class Wizard extends Webform {
   }
 
   checkPageValidity(data, dirty, page) {
-    page = page || this.currentPage;
+    page = page || this.page;
 
     let check = true;
     this.pages[page].forEach((comp) => {
@@ -326,19 +317,38 @@ export default class Wizard extends Webform {
     return super.setForm(form);
   }
 
+  isClickable(page, index) {
+    return this.page !== index && firstNonNil([
+      _.get(page, 'breadcrumbClickable'),
+      this.options.breadcrumbSettings.clickable
+    ]);
+  }
+
   hasButton(name, nextPage) {
+    const currentPage = this.currentPage();
     if (name === 'previous') {
-      return (this.currentPage > 0) && this.options.buttonSettings.showPrevious;
+      const show = firstNonNil([
+        _.get(currentPage, 'buttonSettings.previous'),
+        this.options.buttonSettings.showPrevious
+      ]);
+      return (this.page > 0) && show;
     }
-    nextPage = (nextPage === undefined) ? this.getNextPage(this.submission.data, this.currentPage) : nextPage;
+    nextPage = (nextPage === undefined) ? this.getNextPage(this.submission.data, this.page) : nextPage;
     if (name === 'next') {
-      return (nextPage !== null) && (nextPage < this.pages.length) && this.options.buttonSettings.showNext;
+      const show = firstNonNil([
+        _.get(currentPage, 'buttonSettings.next'),
+        this.options.buttonSettings.showNext
+      ]);
+      return (nextPage !== null) && (nextPage < this.pages.length) && show;
     }
     if (name === 'cancel') {
-      return this.options.buttonSettings.showCancel;
+      return firstNonNil([
+        _.get(currentPage, 'buttonSettings.cancel'),
+        this.options.buttonSettings.showCancel
+      ]);
     }
     if (name === 'submit') {
-      return !this.options.readOnly && ((nextPage === null) || (this.currentPage === (this.pages.length - 1)));
+      return !this.options.readOnly && ((nextPage === null) || (this.page === (this.pages.length - 1)));
     }
     return true;
   }
