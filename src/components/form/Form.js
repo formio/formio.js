f5c6c5589e50b9d21d3af8f875786b76d161540d
Travis Tidwell <travis@form.io>
2019-01-17 13:17:33 -0600

    Fixing issue where nested forms are not hiding submit buttons correctly.

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -206,7 +206,10 @@ export default class FormComponent extends Component {
   filterSubForm() {
     // Iterate through every component and hide the submit button.
     eachComponent(this.component.components, (component) => {
-      if ((component.type === 'button') && (component.action === 'submit')) {
+      if (
+        (component.type === 'button') &&
+        ((component.action === 'submit') || !component.action)
+      ) {
         component.hidden = true;
       }
     });
