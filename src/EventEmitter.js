34902009aca744415db67169e281044a370fcfbb
Ilya Obuhov <iobuhov.mail@gmail.com>
2019-01-23 11:46:52 +0300

    Cleanup

--- src/EventEmitter.js
+++ src/EventEmitter.js
@@ -1,14 +1,10 @@
 import { EventEmitter2 } from 'eventemitter2';
 import * as utils from './utils/utils';
 
-let id = 0;
-
 export default class EventEmitter extends EventEmitter2 {
   constructor(conf = {}) {
     const { loadLimit = 50, eventsSafeInterval = 300, pause = 500, ...ee2conf } = conf;
     super(ee2conf);
-
-    conf.log && console.log(`loadLimit ${loadLimit}`);
 
     const [isPaused, togglePause] = utils.withSwitch(false, true);
 
@@ -17,8 +13,6 @@ export default class EventEmitter extends EventEmitter2 {
       togglePause();
       setTimeout(togglePause, pause);
     };
-
-    this.id = id++;
 
     const dispatch = utils.observeOverload(overloadHandler, {
       limit: loadLimit,
