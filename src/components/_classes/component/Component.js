3a81d79f84f3b7e5a2c4dfa2478183ce4d3a951c
Randall Knutson <randall@knutson.io>
2019-01-30 14:18:19 -0600

    Don't run custom default value in preview mode for default value.

--- src/components/_classes/component/Component.js
+++ src/components/_classes/component/Component.js
@@ -1404,7 +1404,7 @@ export default class Component extends Element {
     if (this.component.defaultValue) {
       defaultValue = this.component.defaultValue;
     }
-    if (this.component.customDefaultValue) {
+    if (this.component.customDefaultValue && !this.options.preview) {
       defaultValue = this.evaluate(
         this.component.customDefaultValue,
         { value: '' },
