e679aeae7c12a9af7b18cef7a45fd1b64cec128e
Randall Knutson <randall@knutson.io>
2019-01-30 14:00:23 -0600

    Fix issue where custom default values aren't applying.

--- src/components/base/Base.js
+++ src/components/base/Base.js
@@ -793,7 +793,7 @@ export default class BaseComponent extends Component {
     if (this.component.defaultValue) {
       defaultValue = this.component.defaultValue;
     }
-    else if (this.component.customDefaultValue) {
+    if (this.component.customDefaultValue) {
       defaultValue = this.evaluate(
         this.component.customDefaultValue,
         { value: '' },
