1a1abf22644a3ef0f0f1316306e790f7f193fa48
Randall Knutson <randall@knutson.io>
2019-03-21 12:24:19 -0500

    Fix layout components sometimes crashing while dragging.

--- src/WebformBuilder.js
+++ src/WebformBuilder.js
@@ -393,7 +393,7 @@ export default class WebformBuilder extends Component {
         return el.classList.contains('drag-copy');
       },
       accepts(el, target) {
-        return !target.classList.contains('no-drop');
+        return !el.contains(target) && !target.classList.contains('no-drop');
       }
     }).on('drop', (element, target, source, sibling) => this.onDrop(element, target, source, sibling));
 
