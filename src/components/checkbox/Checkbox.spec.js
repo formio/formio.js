db77cc17432d017b9ec75374800ce049b3791f24
Cymen Vig <cymenvig@gmail.com>
2019-04-04 12:25:16 -0400

    add test to Checkbox for checkbox-checked class

--- src/components/checkbox/Checkbox.spec.js
+++ src/components/checkbox/Checkbox.spec.js
@@ -41,4 +41,19 @@ describe('Checkbox Component', () => {
       done();
     });
   });
+
+  it('Should set the checkbox-checked class on wrapper of checked checkbox', done => {
+    Harness.testCreate(CheckBoxComponent, comp1).then((component) => {
+      assert(
+        component.element.getAttribute('class').indexOf('checkbox-checked') === -1,
+        'Checkbox wrapper does not have checkbox-checked class.'
+      );
+      Harness.clickElement(component, 'input[type="checkbox"]');
+      assert(
+        component.element.getAttribute('class').indexOf('checkbox-checked') !== -1,
+        'Checkbox wrapper has checkbox-checked class.'
+      );
+      done();
+    });
+  });
 });
