90f70292454dc36e680d0700020908deb299554c
Randall Knutson <randall@knutson.io>
2019-06-03 13:48:22 -0500

    Get custom component editing working.

--- src/components/unknown/Unknown.form.js
+++ src/components/unknown/Unknown.form.js
@@ -1,30 +1,19 @@
-import componentEditForm from '../_classes/component/Component.form';
 import UnknownEditDisplay from './editForm/Unknown.edit.display';
-export default function(...extend) {
-  return componentEditForm([
-    {
-      key: 'display',
-      components: UnknownEditDisplay
-    },
-    {
-      key: 'data',
-      ignore: true
-    },
-    {
-      key: 'validation',
-      ignore: true
-    },
-    {
-      key: 'api',
-      ignore: true
-    },
-    {
-      key: 'conditional',
-      ignore: true
-    },
-    {
-      key: 'logic',
-      ignore: true
-    }
-  ], ...extend);
+export default function() {
+  return {
+    components: [
+      {
+        type: 'tabs',
+        key: 'tabs',
+        components: [
+          {
+            label: 'Custom',
+            key: 'display',
+            weight: 0,
+            components: UnknownEditDisplay
+          }
+        ]
+      }
+    ]
+  };
 }
