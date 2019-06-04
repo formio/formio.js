dd3899ae206a67266bbde64d446786da4f9c8347
Yury Rybak <yuryrybak97@gmail.com>
2019-06-04 12:54:52 +0300

    Fixed min and max words validators.

--- src/components/Validator.js
+++ src/components/Validator.js
@@ -229,7 +229,7 @@ export default {
       message(component, setting) {
         return component.t(component.errorMessage('minWords'), {
           field: component.errorLabel,
-          length: (setting + 1),
+          length: (setting - 1),
           data: component.data
         });
       },
