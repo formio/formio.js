131700de29836f95fbc3fdfe29268bf850704de5
Alex Webb <alexwebb2@gmail.com>
2019-08-01 12:22:33 -0500

    Catch uncaught errors from Webform.submit()

--- src/Webform.js
+++ src/Webform.js
@@ -982,7 +982,10 @@ export default class Webform extends NestedComponent {
     // Clear any existing event handlers in case this is a rebuild
     this.eventHandlers.forEach(h => this.removeEventListener(h.obj, h.type));
 
-    this.on('submitButton', (options) => this.submit(false, options), true);
+    this.on('submitButton', options => {
+      this.submit(false, options).catch(e => e !== false && console.log(e));
+    }, true);
+
     this.on('checkValidity', (data) => this.checkValidity(null, true, data), true);
     this.addComponents(null, null, null, state);
     this.currentForm = this;
@@ -1101,7 +1104,7 @@ export default class Webform extends NestedComponent {
     // Allow for silent cancellations (no error message, no submit button error state)
     if (error && error.silent) {
       this.emit('change', { isValid: true });
-      return;
+      return false;
     }
 
     return this.showErrors(error, true);
