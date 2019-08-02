46c92ea8a7ba9b945859d4fc945052d8f60605c7
Roman Letsuk <romaletsuk@gmail.com>
2019-08-02 14:05:03 +0300

    updated EditGrid component

--- src/components/editgrid/editForm/EditGrid.edit.display.js
+++ src/components/editgrid/editForm/EditGrid.edit.display.js
@@ -3,4 +3,14 @@ export default [
     key: 'placeholder',
     ignore: true,
   },
+  {
+    type: 'checkbox',
+    label: 'Disable Adding / Removing Rows',
+    key: 'disableAddingRemovingRows',
+    tooltip: 'Check if you want to hide Add Another button and Remove Row button',
+    weight: 405,
+    input: true,
+    clearOnHide: false,
+    calculateValue: 'value = data.disableAddingRemovingRows;',
+  },
 ];
