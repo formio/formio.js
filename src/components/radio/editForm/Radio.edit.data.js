6fe8e200035ced4cc10b4ab8e4bf231291cd4cc5
Travis Tidwell <travis@form.io>
2019-09-11 16:38:46 -0500

    Fixed the shortcuts in the builder.

--- src/components/radio/editForm/Radio.edit.data.js
+++ src/components/radio/editForm/Radio.edit.data.js
@@ -1,4 +1,5 @@
 import BuilderUtils from '../../../utils/builder';
+import _ from 'lodash';
 
 export default [
   {
@@ -41,15 +42,11 @@ export default [
         customDefaultValue: () => '',
         template: '{{ item.label }}',
         data: {
-          custom({
-            instance: {
-              root: {
-                editForm,
-                editComponent,
-              } = {},
-            } = {},
-          }) {
-            return BuilderUtils.getAvailableShortcuts(editForm, editComponent);
+          custom(context) {
+            return BuilderUtils.getAvailableShortcuts(
+              _.get(context, 'instance.options.editForm', {}),
+              _.get(context, 'instance.options.editComponent', {})
+            );
           },
         },
       },
