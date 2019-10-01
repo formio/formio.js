c7b4591dd25af80b78a66cf38e956947d3aee16d
Alexander Kiseliov <kiselev20060@gmail.com>
2019-10-01 11:58:44 +0300

    Fixed change event when performing single action.

--- src/components/textarea/TextArea.js
+++ src/components/textarea/TextArea.js
@@ -305,7 +305,8 @@ export default class TextAreaComponent extends TextFieldComponent {
 
     if (this.editorReady) {
       this.editorReady.then((editor) => {
-        this.autoModified = true;
+        this.autoModified = this.autoModified === undefined ? true : this.autoModified;
+
         if (!skipSetting) {
           if (this.component.editor === 'ace') {
             editor.setValue(this.setConvertedValue(value));
