88bea3f897ab1399f75385cc4d78dbeec9e89d7b
Alexey Davydov <alexey@form.io>
2019-10-03 10:24:58 +0300

    Fix switching between widget types for textfield

--- src/components/_classes/widgetcomponent/WidgetComponent.form.js
+++ src/components/_classes/widgetcomponent/WidgetComponent.form.js
@@ -1,8 +1,13 @@
 import baseEditForm from '../component/Component.form';
 import WidgetComponentEditValidation from './editForm/WidgetComponent.edit.validation';
+import WidgetComponentEditDisplay from './editForm/WidgetComponent.edit.display';
 
 export default function(...extend) {
   return baseEditForm([
+    {
+      key: 'display',
+      components: WidgetComponentEditDisplay
+    },
     {
       key: 'validation',
       components: WidgetComponentEditValidation
