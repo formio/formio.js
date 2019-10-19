3eb7024922f9586205096f5e99a7224ec10f0f2e
Travis Tidwell <travis@form.io>
2019-10-18 21:02:27 -0500

    Fixed issue where Select templates could not be overridden.

--- src/WebformBuilder.js
+++ src/WebformBuilder.js
@@ -861,7 +861,7 @@ export default class WebformBuilder extends Component {
       this.preview.form = { components: [_.omit(component, [
         'hidden',
         'conditional',
-        'calculatedValue'
+        'calculateValue'
       ])] };
       const previewElement = this.componentEdit.querySelector('[ref="preview"]');
       if (previewElement) {
@@ -989,7 +989,7 @@ export default class WebformBuilder extends Component {
         'builder',
         'events',
         'attachMode',
-        'calculatedValue'
+        'calculateValue'
       ]));
     }
 
