689bf63ea3f6d66b5060e4c6b737c68d0d07e425
Travis Tidwell <travis@form.io>
2019-10-30 21:07:27 -0500

    Fixing the data map component to allow complex value components.

--- src/components/datagrid/DataGrid.js
+++ src/components/datagrid/DataGrid.js
@@ -194,7 +194,7 @@ export default class DataGridComponent extends NestedComponent {
   }
 
   hasExtraColumn() {
-    return (this.hasRemoveButtons() || this.builderMode);
+    return (this.hasRemoveButtons() || this.canAddColumn);
   }
 
   hasRemoveButtons() {
@@ -217,6 +217,10 @@ export default class DataGridComponent extends NestedComponent {
     return !_.isEqual(newValue, oldValue);
   }
 
+  get canAddColumn() {
+    return this.builderMode;
+  }
+
   render() {
     const columns = this.getColumns();
     return super.render(this.renderTemplate('datagrid', {
@@ -236,6 +240,7 @@ export default class DataGridComponent extends NestedComponent {
       datagridKey: this.datagridKey,
       allowReorder: this.allowReorder,
       builder: this.builderMode,
+      canAddColumn: this.canAddColumn,
       placeholder: this.renderTemplate('builderPlaceholder', {
         position: this.componentComponents.length,
       }),
