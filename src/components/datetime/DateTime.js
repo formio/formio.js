f5bf37ea878143d764c8e6572b141aff04311f0d
Randall Knutson <randall@knutson.io>
2019-11-01 08:50:00 -0500

    Fix flatpickr messing up the default format in certain situations.

--- src/components/datetime/DateTime.js
+++ src/components/datetime/DateTime.js
@@ -125,6 +125,11 @@ export default class DateTimeComponent extends Input {
     return super.isEmpty(value);
   }
 
+  formatValue(input) {
+    const format = FormioUtils.convertFormatToMoment(this.component.format);
+    return moment.utc(input).format(format);
+  }
+
   isEqual(valueA, valueB = this.dataValue) {
     const format = FormioUtils.convertFormatToMoment(this.component.format);
     return (this.isEmpty(valueA) && this.isEmpty(valueB))
