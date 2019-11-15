d5d32f75582069a972cfac8dfa143afaf627af0d
Alexey Davydov <alexey@form.io>
2019-11-15 16:37:17 +0300

    Added bulma template to the list

--- src/templates/index.js
+++ src/templates/index.js
@@ -4,8 +4,10 @@ import bootstrap from './bootstrap';
 // Use external modules instead.
 import bootstrap3 from '@formio/bootstrap3';
 import semantic from '@formio/semantic';
+import bulma from '@formio/bulma';
 export default {
   bootstrap,
   bootstrap3: bootstrap3.templates.bootstrap3,
   semantic: semantic.templates.semantic,
+  bulma: bulma.templates.bulma,
 };
