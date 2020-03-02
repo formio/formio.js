6211e79e17738593fbe5e7c8b47e12a8d9831504
Vlad <vlad.shusterman30@gmail.com>
2020-03-02 13:11:36 +0300

    Feat(HTML): add test on condition

---
+++ src/components/html/fixtures/comp2.js
@@ -0,0 +1,24 @@
+export default {
+  'key': 'html1',
+  'input': false,
+  'tag': 'img',
+  'attrs': [
+    {
+      'value': 'https://www.w3.org/html/logo/downloads/HTML5_Logo_512.png',
+      'attr': 'src'
+    }
+  ],
+  'className': 'thumbnail',
+  'content': '',
+  'type': 'htmlelement',
+  'tags': [
+
+  ],
+  'customConditional':'show = false;',
+  'conditional': {
+    'show': '',
+    'when': '',
+    'eq': ''
+  },
+  'refreshOnChange': true
+};
