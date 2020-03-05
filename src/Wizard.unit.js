4e880feaaae77288acb5f92cae5ea28b65bb7cc8
alexandraRamanenka <sasha@form.io>
2020-03-05 12:18:20 +0300

    Added Wizard test for validation after page was changed with navigation

--- src/Wizard.unit.js
+++ src/Wizard.unit.js
@@ -454,23 +454,56 @@ const wizard = {
       id: 'ebug75q'
     }
   ],
-}
+};
 
 describe('Wizard tests', () => {
   let wizardForm = null;
-  it('Should set components to invalid after page was changed', (done) => {
-    const formElement = document.createElement('div');
-    wizardForm = new Wizard(formElement);
-    wizardForm.setForm(wizard).then(() => {
-      Harness.testSetInput(wizardForm.pages[wizardForm.page].components[0], '1', '1', true);
-      wizardForm.setPage(2).then(() => {
-        wizardForm.setPage(0).then(() => {
-          const aInput = wizardForm.currentPage.getComponent('a');
-          assert.equal(aInput.errors.length, 1);
-          done();
-        }).catch((err) => done(err));
-      });
+  it('Should set components errors if they are after page was changed with navigation', (done) => {
+      const formElement = document.createElement('div');
+      wizardForm = new Wizard(formElement);
+      wizardForm.setForm(wizard).then(() => {
+        Harness.testErrors(wizardForm, {
+          data: {
+            a: '1',
+            c: '',
+            textField: ''
+          }
+        },
+        [{
+          component: 'a',
+          message: 'a must be longer than 3 characters.'
+        }], done);
+        Harness.clickElement(wizardForm, wizardForm.refs[`${wizardForm.wizardKey}-link`][2]);
+        assert.equal(wizardForm.page, 2);
+        Harness.clickElement(wizardForm, wizardForm.refs[`${wizardForm.wizardKey}-link`][0]);
+        assert.equal(wizardForm.page, 0);
+        const aInput = wizardForm.currentPage.getComponent('a');
+        assert.equal(aInput.errors.length, 1);
+        assert.equal(aInput.errors[0].message, 'a must be longer than 3 characters.');
+        done();
     })
     .catch((err) => done(err));
+  });
+
+  it('Should not set components errors if in readOnly mode', (done) => {
+    const formElement = document.createElement('div');
+    wizardForm = new Wizard(formElement, { readOnly: true });
+    wizardForm.setForm(wizard).then(() => {
+      Harness.testSubmission(wizardForm, {
+        data: {
+          a: '1',
+          textField: 'aaa',
+          c: '0'
+        }
+      });
+
+      Harness.clickElement(wizardForm, wizardForm.refs[`${wizardForm.wizardKey}-link`][2]);
+      assert.equal(wizardForm.page, 2);
+      Harness.clickElement(wizardForm, wizardForm.refs[`${wizardForm.wizardKey}-link`][0]);
+      assert.equal(wizardForm.page, 0);
+      const aInput = wizardForm.currentPage.getComponent('a');
+      assert.equal(aInput.errors.length, 0);
+      done();
+    });
   });
 });
