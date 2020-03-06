69a8f757e08a93075cd46991fb0ae0809c011bc1
alexandraRamanenka <sasha@form.io>
2020-03-06 12:41:35 +0300

    Make root form is not pristine before checking visability conditions to delete hidden component value if clearOnHide is turned on

--- src/Webform.js
+++ src/Webform.js
@@ -1246,6 +1246,11 @@ export default class Webform extends NestedDataComponent {
     super.onChange(flags, true);
     const value = _.clone(this.submission);
     flags.changed = value.changed = changed;
+
+    if (modified && this.pristine) {
+      this.pristine = false;
+    }
+
     value.isValid = this.checkData(value.data, flags);
     this.loading = false;
     if (this.submitted) {
@@ -1254,10 +1259,6 @@ export default class Webform extends NestedDataComponent {
     // See if we need to save the draft of the form.
     if (modified && this.options.saveDraft) {
       this.triggerSaveDraft();
-    }
-
-    if (modified && this.pristine) {
-      this.pristine = false;
     }
 
     if (!flags || !flags.noEmit) {
