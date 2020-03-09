c06e956d933bb85e4fc91a02380d720aa2be62cc
Vlad <vlad.shusterman30@gmail.com>
2020-03-09 16:45:28 +0300

    Fix (Datagrid) : add test for disabling childs

--- src/components/datagrid/fixtures/index.js
+++ src/components/datagrid/fixtures/index.js
@@ -1,4 +1,5 @@
 export comp1 from './comp1';
 export comp2 from './comp2';
+export comp3 from './comp3';
 export withDefValue from './comp-with-def-value';
 export withRowGroupsAndDefValue from './comp-row-groups-with-def-value';
