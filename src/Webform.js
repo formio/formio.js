86b9aacf9117db39f5b133bce90ed7c3e3ce32ff
Travis Tidwell <travis@form.io>
2020-04-02 18:42:37 -0500

    Fixing issues with the Disable on Invalid for button components.

--- src/Webform.js
+++ src/Webform.js
@@ -1258,7 +1258,7 @@ export default class Webform extends NestedDataComponent {
     }
 
     if (!flags || !flags.noEmit) {
-      this.emit('change', value);
+      this.emit('change', value, flags);
       isChangeEventEmitted = true;
     }
 
