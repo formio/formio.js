4e4b749860c48ab5157393e813a461383c4ad280
Diana Tsevan <dziyana@form.io>
2020-04-02 11:48:52 +0300

    Focus the first invalid field in pdf form on form submission

--- src/PDF.js
+++ src/PDF.js
@@ -285,17 +285,11 @@ export default class PDF extends Webform {
       this.focusOnComponent(this.errors[0].component.key);
     }
 
+    if (this.errors.length) {
+      this.focusOnComponent(this.errors[0].component.key);
+    }
+
     super.showErrors(error, triggerEvent);
-  }
-
-  setAlert(type, message) {
-    const superSetAlert = super.setAlert(type, message);
-
-    const errorList = document.getElementById(`error-list-${this.id}`);
-    const { left, top } = errorList.getBoundingClientRect();
-    window.scrollTo(left, top);
-
-    return superSetAlert;
   }
 }
 
