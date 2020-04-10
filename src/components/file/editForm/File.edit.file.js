2d0a5d0ead51c07db6cb52218d4d7f93765a3adc
Travis Tidwell <travis@form.io>
2020-04-10 12:17:21 -0500

    Fixing erroneous spaces causing crashes in Chromium

--- src/components/file/editForm/File.edit.file.js
+++ src/components/file/editForm/File.edit.file.js
@@ -93,10 +93,10 @@ export default [
     type: 'textfield',
     input: true,
     key: 'fileKey',
-    label: 'File form-data key',
+    label: 'File form-data key',
     weight: 17,
-    placeholder: 'Enter the key name of a file for form data.',
-    tooltip: 'Key name that you would like to modify for the file while calling API request.',
+    placeholder: 'Enter the key name of a file for form data.',
+    tooltip: 'Key name that you would like to modify for the file while calling API request.',
     conditional: {
       json: {
         '===': [{
