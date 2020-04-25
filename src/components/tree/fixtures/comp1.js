d7207ed27345678e63f096712f9fb37308339cbc
Tanya Gashtold <tanya@form.io>
2020-04-25 18:55:39 +0300

    added tests

---
+++ src/components/tree/fixtures/comp1.js
@@ -0,0 +1,23 @@
+export default {
+  'label': 'Tree',
+  'tableView': true,
+  'calculateServer': false,
+  'key': 'tree',
+  'type': 'tree',
+  'input': true,
+  'tree': true,
+  'components': [{
+    'label': 'Number',
+    'mask': false,
+    'spellcheck': true,
+    'tableView': false,
+    'delimiter': false,
+    'requireDecimal': false,
+    'inputFormat': 'plain',
+    'calculateServer': false,
+    'key': 'number',
+    'type': 'number',
+    'input': true
+  }]
+};
+
