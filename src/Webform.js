a49d8593b474a1028a13369cfcaa0607c3083185
mikekotikov <mikhail@form.io>
2020-05-06 17:32:11 +0300

    Fix (Webform): reset button click didn't hide validation error messages

--- src/Webform.js
+++ src/Webform.js
@@ -997,6 +997,7 @@ export default class Webform extends NestedDataComponent {
   resetValue() {
     _.each(this.getComponents(), (comp) => (comp.resetValue()));
     this.setPristine(true);
+    this.redraw();
   }
 
   /**
