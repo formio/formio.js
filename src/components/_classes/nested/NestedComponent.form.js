f2ce7a9288cffd26d22483a4da8aa1a4fe22223f
Yury Rybak <yuryrybak97@gmail.com>
2020-05-30 17:14:04 +0300

    Fixed import issues.

--- src/components/_classes/nested/NestedComponent.form.js
+++ src/components/_classes/nested/NestedComponent.form.js
@@ -2,11 +2,9 @@ import baseEditForm from '../component/Component.form';
 
 import NestedComponentEditData from './editForm/NestedComponent.edit.data';
 
-export default (...extend) => {
-  return baseEditForm([
-    {
-      key: 'data',
-      components: NestedComponentEditData,
-    },
-  ], ...extend);
-};
+export default (...extend) => baseEditForm([
+  {
+    key: 'data',
+    components: NestedComponentEditData,
+  },
+], ...extend);
