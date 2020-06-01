193791316c03511b3d4f60bc48c3ec9d28b86c22
alexandraRamanenka <sasha@form.io>
2020-06-01 16:47:09 +0300

    Moved draft option to Validation tab

--- src/components/editgrid/editForm/EditGrid.edit.data.js
+++ src/components/editgrid/editForm/EditGrid.edit.data.js
@@ -8,14 +8,6 @@ export default [
     tooltip: 'Check this if you would like your changes within "edit" mode to be committed directly to the submission object as that row is being changed',
   },
   {
-    type: 'checkbox',
-    input: true,
-    weight: 105,
-    key: 'draft',
-    label: 'Draft',
-    tooltip: 'Allow save rows even if it\'s data is invalid. Errors will occur when try to submit with invalid rows.',
-  },
-  {
     key: 'defaultValue',
     ignore: true,
   },
