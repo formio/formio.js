8a0e17d6cc0e72f6815551b8f52254ba0e43c64e
Yury Rybak <yuryrybak97@gmail.com>
2020-06-05 13:11:14 +0300

    Fixed issue with reseting form for Quick Rule.

--- src/components/_classes/component/editForm/utils.js
+++ src/components/_classes/component/editForm/utils.js
@@ -586,14 +586,14 @@ const EditFormUtils = {
             label: 'Add',
             input: true,
             action: 'custom',
-            custom({ form, instance }) {
+            custom({ form }) {
               const helper = new QuickRulesHelper(form.componentEditForm, {});
               const quickRuleInstance = new QuickRule({});
               const inputContainer = form.getComponent(name);
               const input = inputContainer.dataValue;
 
               quickRuleInstance.addRule(helper, input);
-              instance.parent.resetValue();
+              form.resetValue();
             },
           }),
         },
