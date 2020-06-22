991dac846ae7cd02462d8e12f2d4a019464595b7
Travis Tidwell <travis@form.io>
2020-06-21 23:02:44 -0500

    Fixing issues with logging out.

--- src/Formio.js
+++ src/Formio.js
@@ -997,6 +997,7 @@ export default class Formio {
       Formio.tokens = {};
     }
 
+    Formio.tokens[tokenName] = token;
     if (!token) {
       if (!opts.fromUser) {
         opts.fromToken = true;
@@ -1013,8 +1014,6 @@ export default class Formio {
     }
 
     if (Formio.tokens[tokenName] !== token) {
-      // Update the modified token value
-      Formio.tokens[tokenName] = token;
       // iOS in private browse mode will throw an error but we can't detect ahead of time that we are in private mode.
       try {
         localStorage.setItem(tokenName, token);
