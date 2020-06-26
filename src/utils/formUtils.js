3623680ae9f4c6ad14b99f7cac2b9e340943bc85
alexandraRamanenka <sasha@form.io>
2020-06-26 13:40:34 +0300

    Fix (utils): functon was not iterating through columns components

--- src/utils/formUtils.js
+++ src/utils/formUtils.js
@@ -97,7 +97,7 @@ export function eachComponent(components, fn, includeAll, path, parent) {
     if (!noRecurse) {
       if (hasColumns) {
         component.columns.forEach((column) =>
-          eachComponent(column.components, fn, includeAll, subPath(), parent ? component : null));
+          eachComponent(_.isArray(column) ? column : column.components, fn, includeAll, subPath(), parent ? component : null));
       }
 
       else if (hasRows) {
