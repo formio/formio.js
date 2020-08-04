efa01d2d7764ee44f275a9ef8767a178cac8fd30
Travis Tidwell <travis@form.io>
2020-08-03 21:12:44 -0500

    Updated build and version.

--- gulpfile.js
+++ gulpfile.js
@@ -120,13 +120,13 @@ const webpackProd = require('./config/webpack.prod');
 const buildDev = (input, output) => {
   let devConfig = _.cloneDeep(webpackDev);
   devConfig.entry = `./lib/${input}`;
-  devConfig.output.fileName = output;
+  devConfig.output.filename = output;
   return webpackStream(devConfig, webpack).pipe(gulp.dest('dist'));
 };
 const buildProd = (input, output) => {
   let prodConfig = _.cloneDeep(webpackProd);
   prodConfig.entry = `./lib/${input}`;
-  prodConfig.output.fileName = output;
+  prodConfig.output.filename = output;
   return webpackStream(prodConfig, webpack).pipe(gulp.dest('dist'));
 }
 const build = (input, output) => {
