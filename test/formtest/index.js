c84e889773243441f909eb76852ffc06ec74060d
alexandraRamanenka <sasha@form.io>
2020-09-02 13:28:48 +0300

    Added some tests

--- test/formtest/index.js
+++ test/formtest/index.js
@@ -27,6 +27,7 @@ const modalEditComponents = require('./modalEditComponents.json');
 const calculatedNotPersistentValue = require('./calculatedNotPersistentValue.json');
 const initiallyCollapsedPanel = require('./initiallyCollapsedPanel.json');
 const multipleTextareaInsideConditionalComponent = require('./multipleTextareaInsideConditionalComponent.json');
+const disabledNestedForm = require('./disabledNestedForm.json');
 
 module.exports = {
   advanced,
@@ -58,4 +59,5 @@ module.exports = {
   calculatedNotPersistentValue,
   initiallyCollapsedPanel,
   multipleTextareaInsideConditionalComponent,
+  disabledNestedForm,
 };
