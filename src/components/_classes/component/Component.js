60d34786915435b0fd53037603d02dcff4a1808e
AlehKatsiuba <oleg@form.io>
2020-09-04 12:52:49 +0300

    fix data is showing blank when is set to renderMode: 'html', and readOnly: true

--- src/components/_classes/component/Component.js
+++ src/components/_classes/component/Component.js
@@ -2227,6 +2227,10 @@ export default class Component extends Element {
     ) {
       this.redraw();
     }
+    if (this.options.renderMode === 'html') {
+      this.redraw();
+      return changed;
+    }
     for (const i in this.refs.input) {
       if (this.refs.input.hasOwnProperty(i)) {
         this.setValueAt(i, isArray ? value[i] : value, flags);
