b5af41920ea1ba3bbfcceda290f6eca1531abbc8
AlehKatsiuba <oleg@form.io>
2020-09-25 15:38:27 +0300

    Fix case when the blur event is triggered on DateTime Component with custom dateFormat

--- src/widgets/CalendarWidget.js
+++ src/widgets/CalendarWidget.js
@@ -188,7 +188,7 @@ export default class CalendarWidget extends InputWidget {
               // Make sure we commit the value after a blur event occurs.
               this.addEventListener(this.calendar._input, 'blur', (event) => {
                 if (!event.relatedTarget?.className.split(/\s+/).includes('flatpickr-day')) {
-                  this.calendar.setDate(this.calendar.input.value, true, this.settings.altFormat);
+                  this.calendar.setDate(new Date(this.calendar.input.value), true, this.settings.altFormat);
                 }
               });
 
