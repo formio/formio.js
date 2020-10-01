a07fa01bd2af63931c8c41fddd82f4f2c9d9fec2
Tanya Gashtold <tanya@form.io>
2020-10-01 12:27:51 +0300

    fixed failed test

--- test/forms/conditional-nested-form-load.js
+++ test/forms/conditional-nested-form-load.js
@@ -40,7 +40,10 @@ export default {
           data: {
             textField2: 'text',
             checkbox2: false,
-            form: { data: {} }
+            form: { 
+              data: {}, 
+              metadata: {} 
+            }
           }
         });
         expect(fetchMock.calls(url).length, `Step 2: ${ELBC}`).to.equal(0);
@@ -48,7 +51,10 @@ export default {
           data: {
             textField2: '',
             checkbox2: true,
-            form: { data: {} }
+            form: { 
+              data: {}, 
+              metadata: {} 
+            }
           }
         });
         expect(fetchMock.calls(url).length, `Step 3: ${ELBC}`).to.equal(0);
