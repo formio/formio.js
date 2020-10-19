68d3b5fb095d358c18058896fbfb4c9da4bb8f2c
mikekotikov <mikhail@form.io>
2020-10-19 16:40:49 +0300

    EditGrid: add translations to headers

--- src/components/editgrid/EditGrid.js
+++ src/components/editgrid/EditGrid.js
@@ -53,7 +53,7 @@ export default class EditGridComponent extends NestedArrayComponent {
     return `<div class="row">
       {% util.eachComponent(components, function(component) { %}
         {% if (!component.hasOwnProperty('tableView') || component.tableView) { %}
-          <div class="col-sm-2">{{ component.label }}</div>
+          <div class="col-sm-2"><translate>{{component.label}}</translate></div>
         {% } %}
       {% }) %}
     </div>`;
