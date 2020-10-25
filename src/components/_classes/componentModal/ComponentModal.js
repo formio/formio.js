250817bec932ad39fe1e1c780b4de068877e7c36
Marco Tizzoni <marco.tizzoni@gmail.com>
2020-10-25 21:16:50 +0100

    Style button to delete data in confirmation dialog as btn-danger

--- src/components/_classes/componentModal/ComponentModal.js
+++ src/components/_classes/componentModal/ComponentModal.js
@@ -167,7 +167,7 @@ export default class ComponentModal {
       <h3 ref="dialogHeader">${this.component.t('Do you want to clear changes?')}</h3>
       <div style="display:flex; justify-content: flex-end;">
         <button ref="dialogCancelButton" class="btn btn-secondary">${this.component.t('Cancel')}</button>
-        <button ref="dialogYesButton" class="btn btn-primary">${this.component.t('Yes, delete it')}</button>
+        <button ref="dialogYesButton" class="btn btn-danger">${this.component.t('Yes, delete it')}</button>
       </div>
     `;
 
