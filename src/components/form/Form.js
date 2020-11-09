095580d2dc438e26f5ee09515ea334b7c901ad13
alexandraRamanenka <60643585+alexandraRamanenka@users.noreply.github.com>
2020-11-09 13:21:57 +0300

    Fixed an issue where NestedForm's data is not shown when it is inside DataGrid

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -536,7 +536,7 @@ export default class FormComponent extends Component {
   }
 
   isEmpty(value = this.dataValue) {
-    return value === null || _.isEqual(value, this.emptyValue) || this.areAllComponentsEmpty(value.data);
+    return value === null || _.isEqual(value, this.emptyValue) || (this.areAllComponentsEmpty(value.data) && !value._id);
   }
 
   areAllComponentsEmpty(data) {
