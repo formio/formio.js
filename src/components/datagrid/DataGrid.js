b38a9ac93ff7736e4ea5f8a93823a8a1279280b8
mikekotikov <mikhail@form.io>
2021-01-14 16:31:54 +0300

    make DataGrid layout fixed if there is a Select component

--- src/components/datagrid/DataGrid.js
+++ src/components/datagrid/DataGrid.js
@@ -236,7 +236,7 @@ export default class DataGridComponent extends NestedArrayComponent {
 
   render() {
     const columns = this.getColumns();
-    const colWidth = Math.floor(12 / (columns.length + 1)).toString();
+    const layoutFixed = columns.some(col => col.type === 'select') || this.component.layoutFixed;
     return super.render(this.renderTemplate('datagrid', {
       rows: this.getRows(),
       columns: columns,
@@ -259,7 +259,7 @@ export default class DataGridComponent extends NestedArrayComponent {
       placeholder: this.renderTemplate('builderPlaceholder', {
         position: this.componentComponents.length,
       }),
-      colWidth
+      layoutFixed
     }));
   }
 
