fbc1ce572af0b7566d9d1ca8868adef76475615a
alexandraRamanenka <sasha@form.io>
2021-01-27 09:11:24 +0000

    Fixed an issue wher Submit button becomes invalid when there are few components with the same API keys

--- src/WebformBuilder.js
+++ src/WebformBuilder.js
@@ -1173,7 +1173,7 @@ export default class WebformBuilder extends Component {
       }
     });
 
-    this.emit('error', hasInvalidComponents);
+    this.emit('builderError', hasInvalidComponents);
   }
 
   /**
