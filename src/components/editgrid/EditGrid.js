8049ceb790a28167ff6e7c4732f2376cf765132e
mikekotikov <mikhail@form.io>
2021-02-25 19:04:42 +0300

    add prop inEditGrid to components inside it on form use

--- src/components/editgrid/EditGrid.js
+++ src/components/editgrid/EditGrid.js
@@ -818,6 +818,7 @@ export default class EditGridComponent extends NestedArrayComponent {
         row: options.row,
       }), options, row);
       comp.rowIndex = rowIndex;
+      comp.inEditGrid = true;
       return comp;
     });
   }
