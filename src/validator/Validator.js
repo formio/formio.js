d66e69a3dcfda51beaa0901998fc3ec062b6be43
alexandraRamanenka <sasha@form.io>
2021-03-01 12:09:54 +0000

    Fixed an issue where URl validation is not triggered when insert an email

--- src/validator/Validator.js
+++ src/validator/Validator.js
@@ -481,10 +481,12 @@ class ValidationChecker {
           /* eslint-disable max-len */
           // From https://stackoverflow.com/questions/3809401/what-is-a-good-regular-expression-to-match-a-url
           const re = /[-a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#?&//=]*)/;
+          // From http://stackoverflow.com/questions/46155/validate-email-address-in-javascript
+          const emailRe = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
           /* eslint-enable max-len */
 
           // Allow urls to be valid if the component is pristine and no value is provided.
-          return !value || re.test(value);
+          return !value || (re.test(value) && !emailRe.test(value));
         }
       },
       date: {
