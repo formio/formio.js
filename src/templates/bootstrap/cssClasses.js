98fc0fe353c8584883a24b11b4990a5bfb0e986e
Anton K <ntnkonon@gmail.com>
2021-04-06 02:37:31 +0300

    FIO-2648 Added class overrides for dynamic styling with VPAT

--- src/templates/bootstrap/cssClasses.js
+++ src/templates/bootstrap/cssClasses.js
@@ -2,5 +2,14 @@ export default {
   'border-default': '',
   'formio-tab-panel-active': 'active',
   'formio-tab-link-active': 'active',
-  'formio-tab-link-container-active': 'active'
+  'formio-tab-link-container-active': 'active',
+  'formio-form-error': 'formio-error-wrapper has-message',
+  'formio-form-alert': 'alert alert-danger',
+  'formio-label-error': '',
+  'formio-input-error': '',
+  'formio-alert-danger': 'alert alert-danger',
+  'formio-alert-success': 'alert alert-success',
+  'formio-alert-warning': 'alert alert-warning',
+  'formio-modal-cancel-button': 'btn btn-danger',
+  'formio-modal-confirm-button': 'btn-primary',
 };
