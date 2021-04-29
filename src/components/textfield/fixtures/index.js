1180f382004a9d72b983579b11def97ef3c2dd92
alexandraRamanenka <sasha@form.io>
2021-04-29 21:35:38 +0300

    Added tests

--- src/components/textfield/fixtures/index.js
+++ src/components/textfield/fixtures/index.js
@@ -4,3 +4,4 @@ export comp3 from './comp3';
 export comp4 from './comp4';
 export comp5 from './comp5';
 export comp6 from './comp6';
+export withDisplayAndInputMasks from './comp-with-display-and-value-masks';
