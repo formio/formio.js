fe00a3043526d4af05e50b00fad6d253f6856166
DmitryNistratov <dmitry@form.io>
2021-05-05 15:57:32 +0300

    FIO-2936: replace finally method for better compatibility

--- src/Webform.js
+++ src/Webform.js
@@ -1526,8 +1526,11 @@ export default class Webform extends NestedDataComponent {
     this.submitting = true;
     return this.submitForm(options)
       .then(({ submission, saved }) => this.onSubmit(submission, saved))
-      .catch((err) => NativePromise.reject(this.onSubmissionError(err)))
-      .finally(() => this.submissionInProcess = false);
+      .then(() => this.submissionInProcess = false)
+      .catch((err) => {
+        this.submissionInProcess = false;
+        return NativePromise.reject(this.onSubmissionError(err));
+      });
   }
 
   clearServerErrors() {
