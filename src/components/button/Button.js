0e0b11b2dfaa513e1bdc8d119218c014fad78d89
MaksimFalei <Maksim.Falei@softensity.com>
2021-05-12 21:10:08 +0300

    Fixes an issue when for the oAuth redirect URI as host that trigger conflicts with OpenID

--- src/components/button/Button.js
+++ src/components/button/Button.js
@@ -116,6 +116,10 @@ export default class ButtonComponent extends Field {
     return false;
   }
 
+  get isUriFromAction() {
+    return this.component.redirectURIFromSettings;
+  }
+
   render() {
     if (this.viewOnly || this.options.hideButtons) {
       this._visible = false;
@@ -401,7 +405,6 @@ export default class ButtonComponent extends Field {
     };
     /*eslint-enable camelcase */
 
-    // Needs for the correct redirection URI for the OpenID
     const originalRedirectUri = params.redirect_uri;
 
     // Make display optional.
@@ -439,10 +442,14 @@ export default class ButtonComponent extends Field {
           }
           // Depending on where the settings came from, submit to either the submission endpoint (old) or oauth endpoint (new).
           let requestPromise = NativePromise.resolve();
+          // Needs for the correct redirection URI for the OpenID
+          const redirectURI = this.isUriFromAction
+            ? originalRedirectUri
+            : (window.location.origin || `${window.location.protocol}//${window.location.host}`);
 
           if (_.has(this, 'root.form.config.oauth') && this.root.form.config.oauth[this.component.oauthProvider]) {
             params.provider = settings.provider;
-            params.redirectURI = originalRedirectUri;
+            params.redirectURI = redirectURI;
 
             // Needs for the exclude oAuth Actions that not related to this button
             params.triggeredBy = this.key;
@@ -451,7 +458,7 @@ export default class ButtonComponent extends Field {
           else {
             const submission = { data: {}, oauth: {} };
             submission.oauth[settings.provider] = params;
-            submission.oauth[settings.provider].redirectURI = originalRedirectUri;
+            submission.oauth[settings.provider].redirectURI = redirectURI;
 
             // Needs for the exclude oAuth Actions that not related to this button
             submission.oauth[settings.provider].triggeredBy = this.key;
