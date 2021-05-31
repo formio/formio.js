fd9e3cbfbd06211594f429dd6b11cf6462bbd989
DmitryNistratov <dmitry@form.io>
2021-05-31 15:47:32 +0300

    add a test to check the validation of nested forms

---
+++ test/forms/wizardWithFieldsValidationParent.js
@@ -0,0 +1,51 @@
+export default {
+  '_id': '60b0abf1b332da2f881e34f4',
+  'type': 'form',
+  'tags': [],
+  'owner': '6038bed737595d104cfc358a',
+  'components': [
+    {
+      'title': 'Page 1',
+      'label': 'Page 1',
+      'type': 'panel',
+      'key': 'page1',
+      'components': [
+        {
+          'label': 'Form',
+          'tableView': true,
+          // 'form': '60b0aa39b332da2f881e34d7',
+          'useOriginalRevision': false,
+          'key': 'formNested',
+          'type': 'form',
+          'input': true
+        }
+      ],
+      'input': false,
+      'tableView': false
+    }
+  ],
+  'revisions': '',
+  '_vid': 0,
+  'title': 'tr-test-parent',
+  'display': 'wizard',
+  'access': [
+    {
+      'roles': [
+        '6038c83637595d104cfc3594',
+        '6038c83637595d104cfc3595',
+        '6038c83637595d104cfc3596'
+      ],
+      'type': 'read_all'
+    }
+  ],
+  'submissionAccess': [],
+  'controller': '',
+  'properties': {},
+  'settings': {},
+  'name': 'trTestParent',
+  'path': 'trtestparent',
+  'project': '6038c83637595d104cfc3593',
+  'created': '2021-05-28T08:38:09.740Z',
+  'modified': '2021-05-28T08:38:09.746Z',
+  'machineName': 'dqroghuntybetsh:trTestParent'
+};
