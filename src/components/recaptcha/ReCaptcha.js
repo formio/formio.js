aa0aeca0f8a3af63940e519c40cb4e7fdf6b70d6
Travis Tidwell <travis@form.io>
2021-06-17 12:16:55 -0500

    FIO-2888: Fixing the recaptcha so that it will not be reset if it is set.

--- src/components/recaptcha/ReCaptcha.js
+++ src/components/recaptcha/ReCaptcha.js
@@ -25,6 +25,7 @@ export default class ReCaptchaComponent extends Component {
   }
 
   render() {
+    this.recaptchaResult = null;
     if (this.builderMode) {
       return super.render('reCAPTCHA');
     }
@@ -74,15 +75,14 @@ export default class ReCaptchaComponent extends Component {
                   action: actionName
                 })
                 .then((token) => {
-                  return this.sendVerificationRequest(token);
-                })
-                .then(({ verificationResult, token }) => {
-                  this.setValue({
-                    ...verificationResult,
-                    token,
+                  return this.sendVerificationRequest(token).then(({ verificationResult, token }) => {
+                    this.recaptchaResult = {
+                      ...verificationResult,
+                      token,
+                    };
+                    this.updateValue(this.recaptchaResult);
+                    return resolve(verificationResult);
                   });
-
-                  return resolve(verificationResult);
                 });
             });
           })
@@ -130,13 +130,8 @@ export default class ReCaptchaComponent extends Component {
       });
   }
 
-  setValue(value) {
-    const changed = this.hasChanged(value, this.dataValue);
-    this.dataValue = value;
-    return changed;
-  }
-
-  getValue() {
-    return this.dataValue;
+  normalizeValue(newValue) {
+    // If a recaptcha result has already been established, then do not allow it to be reset.
+    return this.recaptchaResult ? this.recaptchaResult : newValue;
   }
 }
