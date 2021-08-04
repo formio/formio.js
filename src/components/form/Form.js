ab952549e04b7d052ceb80943a14ec0481a3f857
TanyaGashtold <tanya@form.io>
2021-08-04 18:32:29 +0300

    FIO-3500: fixed an issue where forms with nested forms inside components with incorrect custom conditions cannot be submitted

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -480,7 +480,7 @@ export default class FormComponent extends Component {
 
   calculateValue(data, flags, row) {
     if (this.subForm) {
-      return this.subForm.calculateValue(this.dataValue.data, flags);
+      return this.subForm.calculateValue(this.dataValue?.data || {}, flags);
     }
 
     return super.calculateValue(data, flags, row);
