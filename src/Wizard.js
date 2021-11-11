b8233462159d8e310607ae2e522acdbd224b21d5
Aliaksandra Ramanenka <A.Ramanenka@itechart-group.com>
2021-11-11 16:25:12 +0300

    Fix(NestedWizard): conditioanlly hidden components of the nested wizard form are not attached to the correct element

--- src/Wizard.js
+++ src/Wizard.js
@@ -457,7 +457,7 @@ export default class Wizard extends Webform {
       const currentComponents = nestedComp?.subForm ? this.getSortedComponents(nestedComp.subForm) : nestedComp?.components || [];
       const visibleComponents = currentComponents.filter(comp => comp._visible);
       const filteredComponents = visibleComponents.filter(comp => !dataArrayComponents.includes(comp.component.type) && (comp.type !== 'form' || comp.isNestedWizard));
-      const additionalComponents = visibleComponents.filter(comp => comp.subForm?._form.display !== 'wizard');
+      const additionalComponents = currentComponents.filter(comp => comp.subForm?._form.display !== 'wizard');
       let hasNested = false;
 
       eachComponent(filteredComponents, (comp) => {
