3c6d28b6eb574f48cdae295687285aa83693b990
darshan <darshan.ks@smartfoodsafe.com>
2021-12-23 12:18:13 +0530

    radoi btn changes

--- src/components/radio/editForm/Radio.edit.data.js
+++ src/components/radio/editForm/Radio.edit.data.js
@@ -1,35 +1,33 @@
-import BuilderUtils from "../../../utils/builder";
-import _ from "lodash";
 
 export default [
   {
-    key: "multiple",
+    key: 'multiple',
     ignore: true,
   },
   {
-    type: "datagrid",
+    type: 'datagrid',
     input: true,
-    label: "Values",
-    key: "values",
+    label: 'Values',
+    key: 'values',
     tooltip:
-      "The radio button values that can be picked for this field. Values are text submitted with the form data. Labels are text that appears next to the radio buttons on the form.",
+      'The radio button values that can be picked for this field. Values are text submitted with the form data. Labels are text that appears next to the radio buttons on the form.',
     weight: 10,
     reorder: true,
-    defaultValue: [{ label: "", value: "" }],
+    defaultValue: [{ label: '', value: '' }],
     components: [
       {
-        label: "Label",
-        key: "label",
+        label: 'Label',
+        key: 'label',
         input: true,
-        type: "textfield",
+        type: 'textfield',
       },
       {
-        label: "Value",
-        key: "value",
+        label: 'Value',
+        key: 'value',
         input: true,
-        type: "textfield",
+        type: 'textfield',
         allowCalculateOverride: true,
-        calculateValue: { _camelCase: [{ var: "row.label" }] },
+        calculateValue: { _camelCase: [{ var: 'row.label' }] },
         validate: {
           required: true,
         },
