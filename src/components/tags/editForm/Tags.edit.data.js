b0853b0706d56558fb556e00aa19dc9b1904776a
KaleVAshwini <72425994+KaleVAshwini@users.noreply.github.com>
2021-12-27 12:37:13 +0530

    modifications

--- src/components/tags/editForm/Tags.edit.data.js
+++ src/components/tags/editForm/Tags.edit.data.js
@@ -20,18 +20,18 @@ export default [
     defaultValue: 0,
     tooltip: 'The maximum amount of tags that can be added. 0 for infinity.'
   },
-  {
-    weight: 24,
-    type: 'selectF',   //select is a custom component , in order to differentiate between form.io 's select component we have used 'selectF'
-    input: true,
-    key: 'storeas',
-    label: 'Store As',
-    dataSrc: 'values',
-    data: {
-      values: [
-        { label: 'String (CSV)', value: 'string' },
-        { label: 'Array of Tags', value: 'array' }
-      ]
-    }
-  }
+  // {
+  //   weight: 24,
+  //   type: 'selectF',   //select is a custom component , in order to differentiate between form.io 's select component we have used 'selectF'
+  //   input: true,
+  //   key: 'storeas',
+  //   label: 'Store As',
+  //   dataSrc: 'values',
+  //   data: {
+  //     values: [
+  //       { label: 'String (CSV)', value: 'string' },
+  //       { label: 'Array of Tags', value: 'array' }
+  //     ]
+  //   }
+  // }
 ];
