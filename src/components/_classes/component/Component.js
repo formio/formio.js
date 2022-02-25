ded0314c1b488be3baed6b160425f528dbce06ab
Hanna Kurban <Hanna.Kurban@itechart-group.com>
2022-02-25 16:07:07 +0300

    View tab shows data when PUT request using POSTMAN

--- src/components/_classes/component/Component.js
+++ src/components/_classes/component/Component.js
@@ -2710,7 +2710,7 @@ export default class Component extends Element {
 
     // Handle all cases when calculated values should not fire.
     if (
-      (this.options.readOnly && !this.options.pdf) ||
+      (this.options.readOnly && !this.options.pdf && !this.component.calculateValue) ||
       !(this.component.calculateValue || this.component.calculateValueVariable) ||
       shouldBeCleared ||
       (this.options.server && !this.component.calculateServer) ||
