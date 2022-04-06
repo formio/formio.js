3ae648963f8442533420d232817b2f566f1dc749
HannaKurban <96909212+HannaKurban@users.noreply.github.com>
2022-04-06 19:19:43 +0300

    FIO-4395: Fixed typos (#4654)
    Co-authored-by: Hanna Kurban <Hanna.Kurban@itechart-group.com>

--- src/components/_classes/component/editForm/Component.edit.data.js
+++ src/components/_classes/component/editForm/Component.edit.data.js
@@ -15,7 +15,7 @@ export default [
     key: 'defaultValue',
     weight: 5,
     placeholder: 'Default Value',
-    tooltip: 'The will be the value for this field, before user interaction. Having a default value will override the placeholder text.',
+    tooltip: 'The Default Value will be the value for this field, before user interaction. Having a default value will override the placeholder text.',
     input: true
   },
   {
