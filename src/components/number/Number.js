371e8eb421b040812c807cf0aa1066b283686942
Travis Tidwell <travis@form.io>
2022-05-23 16:12:03 -0500

    Revert "Number: fix not present in submission if empty"
    This reverts commit 05d808695c4cf1397c8cb73c0a4f7a125965110f.

--- src/components/number/Number.js
+++ src/components/number/Number.js
@@ -97,10 +97,6 @@ export default class NumberComponent extends Input {
     return defaultValue;
   }
 
-  get emptyValue() {
-    return '';
-  }
-
   isDecimalAllowed() {
     return _.get(this.component, 'allowDecimal', !(this.component.validate && this.component.validate.integer));
   }
@@ -149,8 +145,7 @@ export default class NumberComponent extends Input {
     }
 
     const val = this.refs.input[index].value;
-    // Check if just '-' was entered
-    return val && val !== '-_' ? this.parseNumber(val) : this.emptyValue;
+    return val && val !== '-_' ? this.parseNumber(val) : null;
   }
 
   setValueAt(index, value, flags = {}) {
