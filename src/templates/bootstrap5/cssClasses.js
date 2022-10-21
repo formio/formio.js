40def4085636cce6a5f737146a328aa5ed853f11
Anton K <ntnkonon@gmail.com>
2022-10-21 16:17:57 +0300

    Updated templates to remove transform functions

--- src/templates/bootstrap5/cssClasses.js
+++ src/templates/bootstrap5/cssClasses.js
@@ -1,8 +1,3 @@
 export default {
-  'mr-2': 'me-2',
   'form-group': 'formio-form-group',
-  'float-right': 'float-end',
-  'sr-only': 'visually-hidden',
-  'text-right': 'text-end',
-  'pl-0': 'ps-0',
 };
--- src/templates/bootstrap5/index.js
+++ src/templates/bootstrap5/index.js
@@ -1,6 +1,16 @@
+import builderEditForm from './builderEditForm';
 import builderSidebar from './builderSidebar';
 import builderSidebarGroup from './builderSidebarGroup';
-import panel from './panel';
+import builderWizard from './builderWizard';
+import componentModal from './componentModal';
+import datagrid from './datagrid';
+import dialog from './dialog';
+import file from './file';
+import input from './input';
+import label from './label';
+import modalPreview from './modalPreview';
+import radio from './radio';
+import table from './table';
 import cssClasses from './cssClasses';
 
 export default {
@@ -14,8 +24,18 @@ export default {
     }
     return text;
   },
+  builderEditForm,
   builderSidebar,
   builderSidebarGroup,
-  panel,
+  builderWizard,
+  componentModal,
+  datagrid,
+  dialog,
+  file,
+  input,
+  label,
+  modalPreview,
+  radio,
+  table,
   cssClasses
 };
