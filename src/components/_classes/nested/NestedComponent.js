08e6e3f56fd8333afda03295027aa86f6d671cf4
brendanjbond <brendanbond@gmail.com>
2023-01-02 15:01:34 -0600

    insure contextual row data is validated correctly for nested components

--- src/components/_classes/nested/NestedComponent.js
+++ src/components/_classes/nested/NestedComponent.js
@@ -643,6 +643,7 @@ export default class NestedComponent extends Field {
   }
 
   checkValidity(data, dirty, row, silentCheck) {
+    row = this.dataValue || row;
     if (!this.checkCondition(row, data)) {
       this.setCustomValidity('');
       return true;
