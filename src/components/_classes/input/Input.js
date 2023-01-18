cc225a79b115e4d91263b816701f56db0b63f3f6
TanyaGashtold <61136841+TanyaGashtold@users.noreply.github.com>
2023-01-18 19:51:21 +0300

    FIO-5460: fixed an issue where blur and focus events are not fired for (#4944)
    some component (rebased)

--- src/components/_classes/input/Input.js
+++ src/components/_classes/input/Input.js
@@ -1,5 +1,5 @@
 import Multivalue from '../multivalue/Multivalue';
-import { delay, convertStringToHTMLElement } from '../../../utils/utils';
+import { convertStringToHTMLElement } from '../../../utils/utils';
 import Widgets from '../../../widgets';
 import NativePromise from 'native-promise-only';
 import _ from 'lodash';
@@ -316,38 +316,5 @@ export default class Input extends Multivalue {
       }
     }
     this.refs.input = [];
-  }
-
-  addFocusBlurEvents(element) {
-    this.addEventListener(element, 'focus', () => {
-      if (this.root.focusedComponent !== this) {
-        if (this.root.pendingBlur) {
-          this.root.pendingBlur();
-        }
-
-        this.root.focusedComponent = this;
-
-        this.emit('focus', this);
-      }
-      else if (this.root.focusedComponent === this && this.root.pendingBlur) {
-        this.root.pendingBlur.cancel();
-        this.root.pendingBlur = null;
-      }
-    });
-    this.addEventListener(element, 'blur', () => {
-      this.root.pendingBlur = delay(() => {
-        this.emit('blur', this);
-        if (this.component.validateOn === 'blur') {
-          this.root.triggerChange({ fromBlur: true }, {
-            instance: this,
-            component: this.component,
-            value: this.dataValue,
-            flags: { fromBlur: true }
-          });
-        }
-        this.root.focusedComponent = null;
-        this.root.pendingBlur = null;
-      });
-    });
   }
 }
