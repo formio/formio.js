081f9c9baa0969060fb1cbc19dc03838665db014
Alexey Nikipelau <81972123+AlexeyNikipelau@users.noreply.github.com>
2023-01-24 01:32:07 +0300

    added rc version determination for cdn url (#5027)

--- src/Formio.js
+++ src/Formio.js
@@ -1668,6 +1668,9 @@ Formio.version = '---VERSION---';
 Formio.pathType = '';
 Formio.events = new EventEmitter();
 Formio.cdn = new CDN();
+if ((Formio.version || '').includes('rc')) {
+  Formio.cdn.setBaseUrl('https://cdn.test-form.io');
+}
 
 if (typeof global !== 'undefined') {
   Formio.addToGlobal(global);
