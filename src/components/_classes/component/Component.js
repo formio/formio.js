51acc56dd71e09125a7be1fba62416e37b9c1be4
alexandraRamanenka <60643585+alexandraRamanenka@users.noreply.github.com>
2023-02-01 10:20:59 +0200

    FIO-6191: Fixed an issue where addons are not created (#5036)
    * FIO-6191: Fixed an issue where addons are not created
    
    * Fixed typos

--- src/components/_classes/component/Component.js
+++ src/components/_classes/component/Component.js
@@ -532,7 +532,7 @@ export default class Component extends Element {
     }
 
     const settings = addonConfiguration.settings?.data || {};
-    const Addon = Addons[name];
+    const Addon = Addons[name.value];
 
     let addon = null;
 
@@ -545,7 +545,7 @@ export default class Component extends Element {
         this.addons.push(addon);
       }
       else {
-        console.warn(`Addon ${name} does not support component of type ${this.component.type}.`);
+        console.warn(`Addon ${name.label} does not support component of type ${this.component.type}.`);
       }
     }
 
