5d5a6bea4f93252d338266d5f52ba7ceb9762c6e
brendanjbond <brendanbond@gmail.com>
2023-05-28 15:23:38 -0500

    manually integrate changes from commit 9273381afa5311f4316b5f1c3823b4732192ae42 (orphaned 5.x renderer)

--- src/components/editgrid/EditGrid.js
+++ src/components/editgrid/EditGrid.js
@@ -1042,6 +1042,15 @@ export default class EditGridComponent extends NestedArrayComponent {
   }
 
   createRowComponents(row, rowIndex, recreatePartially) {
+    // Iterate through existing components and destroy the ones with the same rowIndex.
+    if (this.components) {
+      for (let i = 0; i < this.components.length; i++) {
+        if (this.components[i].rowIndex === rowIndex) {
+          this.components[i].destroy();
+          this.components.splice(i, 1);
+        }
+      }
+    }
     const currentRowComponents = _.get(this.editRows, `[${rowIndex}].components`, null);
     return this.component.components.map((col, colIndex) => {
       if (recreatePartially && currentRowComponents && this.variableTypeComponentsIndexes.length) {
