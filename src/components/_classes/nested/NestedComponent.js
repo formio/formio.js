2f942f39197e89595e209d9052968835fec45569
brendanjbond <brendanbond@gmail.com>
2023-06-02 11:11:49 -0500

    Squashed commit of the following:
    commit 6772bceb203004b0f9c61ab1fe6b0d55f00341a4
    Author: brendanjbond <brendanbond@gmail.com>
    Date:   Thu Jun 1 16:49:09 2023 -0500
    
        fix tests
    
    commit 730467156da5d64b377c16a968dc52294cc10bc6
    Author: brendanjbond <brendanbond@gmail.com>
    Date:   Thu Jun 1 14:35:34 2023 -0500
    
        WIP: fixing the tests
    
    commit 0ce3df947dc26f4eed8378a2eb2072a02a727b73
    Author: brendanjbond <brendanbond@gmail.com>
    Date:   Thu Jun 1 14:23:00 2023 -0500
    
        fix editgrid test
    
    commit b91b85015679d37aae67e6f9c4d9cef5e0a4e2c9
    Author: brendanjbond <brendanbond@gmail.com>
    Date:   Thu Jun 1 14:22:27 2023 -0500
    
        add lib to eslint ignore
    
    commit 1af3f3309e851dce006b73be089c3051e956ae19
    Author: brendanjbond <brendanbond@gmail.com>
    Date:   Wed May 31 10:54:56 2023 -0500
    
        fix jekyll endpoint
    
    commit 8d5e0cc7f14eceb630492cb70d1db458767920c2
    Author: brendanjbond <brendanbond@gmail.com>
    Date:   Wed May 31 10:34:53 2023 -0500
    
        remove docs and dist from repo after ignoring
    
    commit d3e147efc98b54972af72b7dec3861c7c033b85d
    Author: brendanjbond <brendanbond@gmail.com>
    Date:   Wed May 31 10:21:41 2023 -0500
    
        add docs to gitignore
    
    commit 4bb0ffe8b16303a071f5df3eef592cf9be1a5afb
    Author: brendanjbond <brendanbond@gmail.com>
    Date:   Wed May 31 10:20:47 2023 -0500
    
        add dist to gitignore
    
    commit 5d5a6bea4f93252d338266d5f52ba7ceb9762c6e
    Author: brendanjbond <brendanbond@gmail.com>
    Date:   Sun May 28 15:23:38 2023 -0500
    
        manually integrate changes from commit 9273381afa5311f4316b5f1c3823b4732192ae42 (orphaned 5.x renderer)
    
    commit f4640d9399b1d54cb21ec1352b4a06e6f85f01a0
    Author: brendanjbond <brendanbond@gmail.com>
    Date:   Sun May 28 15:20:51 2023 -0500
    
        manually integrate changes from commit d9c4b97102ba7461781e1532ef5e786240ebd143 (orphaned 5.x renderer)
    
    commit 63bcb8047660ac7373edd8e1c1e8c7d0e27a10eb
    Author: brendanjbond <brendanbond@gmail.com>
    Date:   Sun May 28 15:12:44 2023 -0500
    
        manually integrate changes from commit 248ca81ccb6cd538ed700fa1ebe1abfde9e3358d (orphaned 5.x renderer)
    
    commit 78d53600386f844d03a62d312c4b30394f5453f4
    Author: brendanjbond <brendanbond@gmail.com>
    Date:   Sun May 28 15:09:39 2023 -0500
    
        manually integrate final form changes from commit 94fa4810cb2c59f620f1757e70d77eab79379448 (orphaned 5.x renderer)
    
    commit 3c151d5dd508b06c6eea4edfb5006bb5dba124fa
    Author: brendanjbond <brendanbond@gmail.com>
    Date:   Sun May 28 15:03:53 2023 -0500
    
        manually integrate changes from commit 94fa4810cb2c59f620f1757e70d77eab79379448 (orphaned 5.x renderer)

--- src/components/_classes/nested/NestedComponent.js
+++ src/components/_classes/nested/NestedComponent.js
@@ -393,6 +393,7 @@ export default class NestedComponent extends Field {
    */
   addComponents(data, options) {
     data = data || this.data;
+    this.components = this.components || [];
     options = options || this.options;
     if (options.components) {
       this.components = options.components;
@@ -413,6 +414,7 @@ export default class NestedComponent extends Field {
    */
   addComponent(component, data, before, noAdd) {
     data = data || this.data;
+    this.components = this.components || [];
     if (this.options.parentPath) {
       component.shouldIncludeSubFormPath = true;
     }
@@ -523,9 +525,9 @@ export default class NestedComponent extends Field {
    * @param {Component} component - The component to remove from the components.
    * @param {Array<Component>} components - An array of components to remove this component from.
    */
-  removeComponent(component, components) {
+  removeComponent(component, components, all = false) {
     components = components || this.components;
-    component.destroy();
+    component.destroy(all);
     _.remove(components, { id: component.id });
   }
 
@@ -723,14 +725,14 @@ export default class NestedComponent extends Field {
     super.clear();
   }
 
-  destroy() {
-    this.destroyComponents();
-    super.destroy();
+  destroy(all = false) {
+    this.destroyComponents(all);
+    super.destroy(all);
   }
 
-  destroyComponents() {
+  destroyComponents(all = false) {
     const components = this.getComponents().slice();
-    components.forEach((comp) => this.removeComponent(comp, this.components));
+    components.forEach((comp) => this.removeComponent(comp, this.components, all));
     this.components = [];
   }
 
