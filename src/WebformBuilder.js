5289deb0945fc210932c8fcee4f25233c6eea1ed
alexandraRamanenka <60643585+alexandraRamanenka@users.noreply.github.com>
2023-07-27 17:10:28 +0300

    FIO-7043: Fixes an issue where {{}} values in DataSource headers are resolved in the form builder (#5287)

--- src/WebformBuilder.js
+++ src/WebformBuilder.js
@@ -1476,7 +1476,9 @@ export default class WebformBuilder extends Component {
         }
       ]
     } : ComponentClass.editForm(_.cloneDeep(overrides));
-    const instanceOptions = {};
+    const instanceOptions = {
+      inFormBuilder: true,
+    };
 
     this.hook('instanceOptionsPreview', instanceOptions);
 
