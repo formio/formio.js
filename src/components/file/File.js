702cda0fef31700fb9231e12743bfcc9812c87d2
llaurentiu <laurentiu.latu@gmail.com>
2023-07-31 17:53:16 +0300

    [BUG] File upload (multi) - aborting upload always cancels the last one in the list (FIO-4905) (#5293)
    * abort the correct file when clicking the cancel upload button
    
    abort the correct file when clicking the cancel upload button
    
    * fix unit test. remove aborted file from list of uploading files
    
    ---------
    
    Co-authored-by: Travis Tidwell <travis@form.io>

--- src/components/file/File.js
+++ src/components/file/File.js
@@ -419,9 +419,14 @@ export default class FileComponent extends Field {
     this.refs.fileStatusRemove.forEach((fileStatusRemove, index) => {
       this.addEventListener(fileStatusRemove, 'click', (event) => {
         event.preventDefault();
-        if (this.abortUpload) {
-          this.abortUpload();
+
+        const fileUpload = this.statuses[index];
+        _.pull(this.filesUploading, fileUpload.originalName);
+
+        if (fileUpload.abort) {
+          fileUpload.abort();
         }
+
         this.statuses.splice(index, 1);
         this.redraw();
       });
@@ -649,6 +654,7 @@ export default class FileComponent extends Field {
         const fileName = uniqueName(file.name, this.component.fileNameTemplate, this.evalContext());
         const escapedFileName = file.name ? file.name.replaceAll('<', '&lt;').replaceAll('>', '&gt;') : file.name;
         const fileUpload = {
+          abort: () => null,
           originalName: escapedFileName,
           name: fileName,
           size: file.size,
@@ -704,7 +710,6 @@ export default class FileComponent extends Field {
             pattern: this.component.filePattern,
           });
         }
-
         // Check file minimum size
         if (this.component.fileMinSize && !this.validateMinSize(file, this.component.fileMinSize)) {
           fileUpload.status = 'error';
@@ -758,7 +763,6 @@ export default class FileComponent extends Field {
               });
             }
           });
-
           const fileKey = this.component.fileKey || 'file';
           const groupResourceId = groupKey ? this.currentForm.submission.data[groupKey]._id : null;
           let processedFile = null;
@@ -785,7 +789,7 @@ export default class FileComponent extends Field {
             }
           }
 
-          fileUpload.message = this.t('Starting upload.');
+          fileUpload.message = this.t('Starting upload...');
           this.redraw();
 
           const filePromise = fileService.uploadFile(
@@ -809,8 +813,7 @@ export default class FileComponent extends Field {
             () => {
               this.emit('fileUploadingStart', filePromise);
             },
-            // Abort upload callback
-            (abort) => this.abortUpload = abort,
+            (abort) => fileUpload.abort = abort,
           ).then((fileInfo) => {
               const index = this.statuses.indexOf(fileUpload);
               if (index !== -1) {
