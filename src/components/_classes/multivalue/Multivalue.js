67c766f62161468e5aec9e87c94e555f535b4515
alexandraRamanenka <sasha@form.io>
2023-08-07 12:52:35 +0200

    FIO-6578: Fixes an issue with losing focus on Year field when Day component has advanced logic

--- src/components/_classes/multivalue/Multivalue.js
+++ src/components/_classes/multivalue/Multivalue.js
@@ -215,20 +215,6 @@ export default class Multivalue extends Field {
     }
   }
 
-  // Saves current caret position to restore it after the component is redrawn
-  saveCaretPosition(element, index) {
-    if (this.root?.focusedComponent?.path === this.path) {
-      try {
-        this.root.currentSelection = { selection: [element.selectionStart, element.selectionEnd], index };
-      }
-      catch (e) {
-        if (!(e instanceof DOMException)) {
-          console.debug(e);
-        }
-      }
-    }
-  }
-
   onSelectMaskHandler(event) {
     this.updateMask(event.target.maskInput, this.getMaskPattern(event.target.value));
   }
