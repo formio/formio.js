22b32cfadbbb02bcdc919403951bcaecca0c2202
alexandraRamanenka <sasha@form.io>
2023-09-26 16:19:04 +0300

    Fixed failing formio tests

--- src/Wizard.js
+++ src/Wizard.js
@@ -912,6 +912,13 @@ export default class Wizard extends Webform {
       return this.setNestedValue(page, submission.data, flags, changed) || changed;
     }, false);
 
+    if (!flags.sanitize ||
+      (flags && flags.fromSubmission && (this.prefixComps.length || this.suffixComps.length) && submission._id) ||
+      (this.options.server && (this.prefixComps.length || this.suffixComps.length))
+    ) {
+      this.mergeData(this.data, submission.data);
+    }
+
     if (changed) {
       this.pageFieldLogic(this.page);
     }
