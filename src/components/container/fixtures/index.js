75718e40759e1dec26bfb2448dd796dfbb5d00ad
alexandraRamanenka <sasha@form.io>
2023-11-10 14:43:27 +0200

    FIO-7547: Container hidden with conditional logic still appears in submission

--- src/components/container/fixtures/index.js
+++ src/components/container/fixtures/index.js
@@ -1,4 +1,5 @@
 import comp1 from './comp1';
 import comp2 from './comp2';
 import comp3 from './comp3';
-export { comp1, comp2, comp3 };
+import comp4 from './comp4';
+export { comp1, comp2, comp3, comp4 };
