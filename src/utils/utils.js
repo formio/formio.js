f0eff65fe655a06bc418a634763e63b4be1b19dd
Hanna Kurban <hannak@form.io>
2024-01-24 18:18:27 +0300

    FIO-7809 fixed pdf submission download error

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -36,8 +36,7 @@ jsonLogic.add_operation('relativeMaxDate', (relativeMaxDate) => {
   return moment().add(relativeMaxDate, 'days').toISOString();
 });
 
-export { jsonLogic, ConditionOperators };
-export * as moment from 'moment-timezone/moment-timezone';
+export { jsonLogic, moment, ConditionOperators };
 
 function setPathToComponentAndPerentSchema(component) {
   component.path = getComponentPath(component);
