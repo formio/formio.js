d7f19286e4c9062dc5406eee06707824bba8bb3d
alexandraRamanenka <60643585+alexandraRamanenka@users.noreply.github.com>
2024-01-31 11:37:04 -0500

    FIO-7764: Fixes a typo in File component settings (#5480)

--- src/components/file/editForm/File.edit.file.js
+++ src/components/file/editForm/File.edit.file.js
@@ -185,7 +185,7 @@ export default [
     input: true,
     key: 'fileNameTemplate',
     label: 'File Name Template',
-    placeholder: '(optional) { {name} }-{ {guid} }"',
+    placeholder: '(optional) { {name} }-{ {guid} }',
     tooltip: 'Specify template for name of uploaded file(s). Regular template variables are available (`data`, `component`, `user`, `value`, `moment` etc.), also `fileName`, `guid` variables are available. `guid` part must be present, if not found in template, will be added at the end.',
     weight: 25
   },
