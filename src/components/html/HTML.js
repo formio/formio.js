94a5edd2552f5bd7fd42a8629758b0b67e32a958
lane-formio <lane@form.io>
2024-02-01 12:58:21 -0600

    Revert "FIO-7544: Fixes an issue where scripts inside HTML component will be …"
    This reverts commit cde5f97937244694997456e2ebf7e10d7c17e60e.

--- src/components/html/HTML.js
+++ src/components/html/HTML.js
@@ -45,15 +45,13 @@ export default class HTMLComponent extends Component {
     }
 
     const submission = _.get(this.root, 'submission', {});
-    const content = this.component.content ? this.interpolate(
-      this.sanitize(this.component.content, this.shouldSanitizeValue),
-      {
-        metadata: submission.metadata || {},
-        submission: submission,
-        data: this.rootValue,
-        row: this.data
+    const content = this.component.content ? this.interpolate(this.component.content, {
+      metadata: submission.metadata || {},
+      submission: submission,
+      data: this.rootValue,
+      row: this.data
     }) : '';
-    return content;
+    return this.sanitize(content, this.shouldSanitizeValue);
   }
 
   get singleTags() {
