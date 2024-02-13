b3a9a82769b24e18591a014995dbc9368e4c6ad2
TanyaGashtold <61136841+TanyaGashtold@users.noreply.github.com>
2024-02-13 09:43:58 +0300

    FIO-7808: fixed an issue where conditional editGrid loses the value after submission (#5511)

--- src/Webform.unit.js
+++ src/Webform.unit.js
@@ -194,7 +194,6 @@ describe('Webform tests', function() {
     Formio.makeRequest = function() {
       return new Promise((res, rej) => {
         setTimeout(() => {
-          console.log(8888);
           rej(errorText);
         }, 50);
       });
