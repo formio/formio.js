fe8fd60f5683114e1d8bfbaea6e468d53262ff23
Hanna Kurban <hannak@form.io>
2024-04-02 15:17:42 +0300

    FIO-8109 fixed save draft triggeribg for nested components

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -165,6 +165,7 @@ export default class FormComponent extends Component {
     }
   }
 
+  /* eslint-disable max-statements */
   getSubOptions(options = {}) {
     options.parentPath = `${this.path}.data.`;
     options.events = this.createEmitter();
@@ -222,12 +223,14 @@ export default class FormComponent extends Component {
     }
     if (this.options.saveDraft) {
       options.saveDraft = this.options.saveDraft;
+      options.formio = new Formio(this.formSrc);
     }
     if (this.options.saveDraftThrottle) {
       options.saveDraftThrottle = this.options.saveDraftThrottle;
     }
     return options;
   }
+  /* eslint-enable max-statements */
 
   render() {
     if (this.builderMode) {
@@ -454,10 +457,6 @@ export default class FormComponent extends Component {
         this.subForm.nosubmit = true;
         this.subForm.root = this.root;
         this.subForm.localRoot = this.isNestedWizard ? this.localRoot : this.subForm;
-        if (this.parent) {
-          this.subForm.draftEnabled = this.parent.draftEnabled;
-          this.subForm.savingDraft = this.parent.savingDraft;
-        }
         this.restoreValue();
         this.valueChanged = this.hasSetValue;
         this.onChange();
