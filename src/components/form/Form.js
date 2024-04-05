d33e3858deb5233dd3517ee0e670eb2b0b0e7abf
HannaKurban <96909212+HannaKurban@users.noreply.github.com>
2024-04-05 10:15:05 +0300

    FIO-8146 fixed saveDraft triggering for nested forms after submitting (#5556)
    * FIO-8146 fixed saveDraft triggering for nested forms after submitting
    
    * FIO-8146 refactoring

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -644,6 +644,10 @@ export default class FormComponent extends Component {
    */
   beforeSubmit() {
     const submission = this.dataValue;
+    // Cancel triggered saveDraft
+    if (this.subForm?.draftEnabled && this.subForm.triggerSaveDraft?.cancel) {
+      this.subForm.triggerSaveDraft.cancel();
+    }
 
     const isAlreadySubmitted = submission && submission._id && submission.form;
 
