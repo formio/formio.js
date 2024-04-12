b27d9d2b6d8e58971e097e1fb8442c2f1f4edf8a
alexandraRamanenka <60643585+alexandraRamanenka@users.noreply.github.com>
2024-04-12 12:51:39 +0300

    FIO-3703: Fixes an issue where NestedData components with modal view do not render values inside Layout components in modal preview table (#5558)
    * FIO-3703: Fixes an issue where NestedData components with modal view do not render values inside Layout components in modal preview table
    
    * Fixed tests
    
    * Fixed typo
    
    * Update NestedDataComponent.unit.js

--- src/components/_classes/nesteddata/NestedDataComponent.unit.js
+++ src/components/_classes/nesteddata/NestedDataComponent.unit.js
@@ -1,6 +1,9 @@
 'use strict';
+import { Formio } from '../../../Formio';
 import NestedDataComponent from './NestedDataComponent';
 import Harness from '../../../../test/harness';
+import assert from 'power-assert';
+import nestedDataCompWithModalPreview from '../../../../test/forms/nestedDataWithModalViewAndLayoutComponents';
 
 let component = null;
 describe('NestedDataComponent class', () => {
@@ -24,5 +27,30 @@ describe('NestedDataComponent class', () => {
       Harness.testElements(component, 'input[name="data[firstName]"]', 1);
       Harness.testElements(component, 'input[name="data[lastName]"]', 1);
     });
+  });
+  it('Should show preview of the modal view component properly', (done) => {
+    Formio.createForm(document.createElement('div'), nestedDataCompWithModalPreview)
+      .then((form) => {
+        const openModalBtn = form.element.querySelector('.open-modal-button');
+        const openModalBtnRows = openModalBtn.querySelectorAll('tr');
+        assert.equal(openModalBtnRows.length, 1);
+
+        const dataGrid = form.getComponent(['dataGrid']);
+        dataGrid.setValue([
+          {
+            textField: 'test'
+          },
+          {
+            textField: 'test2'
+          }
+        ]);
+
+        setTimeout(() => {
+          const modalPreviewValues = form.element.querySelectorAll('.open-modal-button tr td input');
+          assert.equal(modalPreviewValues.length, 2);
+          assert.deepEqual(Array.prototype.map.call(modalPreviewValues, (i) => i.value), ['test', 'test2']);
+          done();
+        }, 300);
+      }).catch(done);
   });
 });
