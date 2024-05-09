17ce18829b373255594781e9c10d51f5e6c01f06
brendanjbond <brendanbond@gmail.com>
2024-05-09 13:57:59 -0500

    update formbuilder options; type listcomponent and child classes

--- src/FormBuilder.js
+++ src/FormBuilder.js
@@ -9,11 +9,11 @@ export default class FormBuilder extends Form {
    * @property {boolean} [noNewEdit] - When set to TRUE no modal is shown when a component is dragged onto the form.
    * @property {boolean} [noDefaultSubmitButton] - Set to TRUE to not include the default submit button in Webforms.
    * @property {boolean} [alwaysConfirmComponentRemoval] - Set to TRUE to always require confirmation before removing a component.
-   * @property {Object} [formConfig] - Form configurations to apply to forms being created. These configurations are added to the "config" property of the form object.
+   * @property {object} [formConfig] - Form configurations to apply to forms being created. These configurations are added to the "config" property of the form object.
    * @property {string} [resourceTag] - The tag to use to query for the "Existing Resource Fields" section of the builder.
    * @property {import('./Form').FormOptions} [editForm] - The options to apply to the Edit Form (the form that shows inside the modal when you edit a component).
    * @property {string} [language] - The language to load into the form builder.
-   * @property {Object} [builder] - The builder options to pass to the builder.
+   * @property {object} [builder] - The builder options to pass to the builder.
    * @property {'form'|'wizard'|'pdf'} [display] - The display mode of the builder.
    * @property {string} [resourceFilter] - Filter applied to the resources that appear in the builder's Existing Resource Fields.
    * @property {boolean} [noSource] - When set to TRUE, the resource ID in the builder's Existing Resource Fields will not be linked.
@@ -29,10 +29,9 @@ export default class FormBuilder extends Form {
   /**
    * Creates a new form builder.
    * @param {HTMLElement} element - The HTML element to place the form builder.
-   * @param {string|Object} form - The form to pass to the builder
+   * @param {string | object} form - The form to pass to the builder
    * @param {FormBuilderOptions} options - The options to create this builder.
-   * @return {FormBuilder} - The form builder instance.
-   *
+   * @returns {FormBuilder} - The form builder instance.
    */
   constructor(element, form, options) {
     form = form || {};
