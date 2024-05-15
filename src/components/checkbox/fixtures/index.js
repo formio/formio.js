314e2599ce3b82f218b365e1be864ad03992c083
HannaKurban <96909212+HannaKurban@users.noreply.github.com>
2024-05-15 11:48:51 +0300

    FIO-7595 fixed incorrect value for conditionally hidden Checkbox (#5594)

--- src/components/checkbox/fixtures/index.js
+++ src/components/checkbox/fixtures/index.js
@@ -3,4 +3,5 @@ import customDefaultComponent from './customDefaultComponent';
 import comp2 from './comp2';
 import comp3 from './comp3';
 import comp4 from './comp4';
-export { comp1, comp2, comp3, comp4, customDefaultComponent };
+import comp5 from './comp5';
+export { comp1, comp2, comp3, comp4, comp5, customDefaultComponent };
