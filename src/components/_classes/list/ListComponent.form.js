5622ef91b34c9be2d5685d409cba49ba79ce8190
brendanjbond <brendanbond@gmail.com>
2024-05-16 14:22:04 -0400

    jsdoc updates for Well.form and ListComponent.form

--- src/components/_classes/list/ListComponent.form.js
+++ src/components/_classes/list/ListComponent.form.js
@@ -2,9 +2,9 @@ import Components from '../../Components';
 import ListEditData from './editForm/ListComponent.edit.data';
 
 /**
- * List Component edit form.
+ * List Component schema.
  * @param {...any} extend - The extended schema.
- * @returns {object} - The List Component edit form.
+ * @returns {object} - The List Component schema.
  */
 export default function(...extend) {
   return Components.baseEditForm([
--- src/components/well/Well.form.js
+++ src/components/well/Well.form.js
@@ -3,8 +3,9 @@ import nestedComponentForm from '../_classes/nested/NestedComponent.form';
 import WellEditDisplay from './editForm/Well.edit.display';
 
 /**
- *
- * @param {...any} extend
+ * Well Component schema.
+ * @param {...any} extend - The extended schema.
+ * @returns {object} - The Well Component schema.
  */
 export default function(...extend) {
   return nestedComponentForm([
