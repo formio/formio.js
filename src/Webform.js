ecc902f3a3c895add3edfec554be7311c0c894e4
HannaKurban <96909212+HannaKurban@users.noreply.github.com>
2024-05-16 17:26:39 +0300

    FIO-8342 fixed triggering saveDraft after submission form (#5600)

--- src/Webform.js
+++ src/Webform.js
@@ -867,7 +867,7 @@ export default class Webform extends NestedDataComponent {
     const draft = fastCloneDeep(this.submission);
     draft.state = 'draft';
 
-    if (!this.savingDraft) {
+    if (!this.savingDraft && !this.submitting) {
       this.emit('saveDraftBegin');
       this.savingDraft = true;
       this.formio.saveSubmission(draft).then((sub) => {
