cd3efea567497e17d9a2aec73fe03c85570e7f48
alexandraRamanenka <60643585+alexandraRamanenka@users.noreply.github.com>
2024-05-20 13:58:19 +0300

    Added RexExp description

--- src/WebformBuilder.js
+++ src/WebformBuilder.js
@@ -1265,6 +1265,7 @@ export default class WebformBuilder extends Component {
   highlightInvalidComponents() {
     const repeatablePaths = this.findRepeatablePaths();
     let hasInvalidComponents = false;
+    // Matches anything expect letters and  '_' at the begginnig of the key and anything except of letters, numbers, '-', '.' and '_' in the rest of the key
     const badCharacters = /^[^A-Za-z_]+|[^A-Za-z0-9\-._]+/g;
 
     this.webform.everyComponent((comp) => {
