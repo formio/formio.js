da995d05200da831627aada7cc386dea4c9e545c
lane-formio <lane@form.io>
2024-05-21 13:25:25 -0500

    Fix merge conflict error

--- src/components/form/fixtures/index.js
+++ src/components/form/fixtures/index.js
@@ -5,5 +5,5 @@ export comp3 from './comp3';
 export comp4 from './comp4';
 export comp5 from './comp5';
 export comp6 from './comp6';
-export comp6 from './comp7';
-export comp6 from './comp8';
+export comp7 from './comp7';
+export comp8 from './comp8';
