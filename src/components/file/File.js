f9208f74d6e1570b6f81d087d1a3a0aa8b02cee1
ZenMasterJacob20011 <jsnichols0404@hotmail.com>
2024-06-11 09:16:47 -0500

    fixed file component upload error when no defaults is set to true

--- src/components/file/File.js
+++ src/components/file/File.js
@@ -744,7 +744,7 @@ export default class FileComponent extends Field {
     const fileWithSameNameUploading = this.filesToSync.filesToUpload
       .some(fileToSync => fileToSync.file?.name === file.name);
 
-    const fileWithSameNameUploaded = this.dataValue
+    const fileWithSameNameUploaded = this.dataValue && this.dataValue
       .some(fileStatus => fileStatus.originalName === file.name);
 
     return fileWithSameNameUploaded || fileWithSameNameUploading
