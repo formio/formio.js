fbb0318c3d5930f35fb0699368f2f8ca81a816f7
mikekotikov <kmikecoder@gmail.com>
2024-06-13 17:59:31 +0300

    FIO-8414: Add test

---
+++ src/components/datagrid/fixtures/comp9.js
@@ -0,0 +1,86 @@
+export default {
+  title: 'randomName',
+  name: 'randomName',
+  path: 'randomName',
+  type: 'form',
+  display: 'form',
+  components: [
+    {
+      label: 'Data Grid',
+      reorder: false,
+      addAnotherPosition: 'bottom',
+      layoutFixed: false,
+      enableRowGroups: false,
+      initEmpty: false,
+      tableView: false,
+      defaultValue: [
+        {}
+      ],
+      validate: {
+        required: true
+      },
+      key: 'dataGrid',
+      type: 'datagrid',
+      input: true,
+      components: [
+        {
+          label: 'Columns',
+          columns: [
+            {
+              components: [
+                {
+                  label: 'Text Field',
+                  applyMaskOn: 'change',
+                  tableView: true,
+                  key: 'textField',
+                  type: 'textfield',
+                  input: true
+                }
+              ],
+              width: 6,
+              offset: 0,
+              push: 0,
+              pull: 0,
+              size: 'md',
+              currentWidth: 6
+            },
+            {
+              components: [
+                {
+                  label: 'Checkbox',
+                  tableView: false,
+                  key: 'checkbox',
+                  type: 'checkbox',
+                  input: true
+                }
+              ],
+              width: 6,
+              offset: 0,
+              push: 0,
+              pull: 0,
+              size: 'md',
+              currentWidth: 6
+            }
+          ],
+          key: 'columns',
+          type: 'columns',
+          input: false,
+          tableView: false
+        }
+      ]
+    },
+    {
+      type: 'button',
+      label: 'Submit',
+      key: 'submit',
+      disableOnInvalid: true,
+      input: true,
+      tableView: false
+    }
+  ],
+  pdfComponents: [],
+  settings: {
+    logs: 'true'
+  }
+
+};
