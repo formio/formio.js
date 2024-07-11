1ed7ab8848a996e0d053b23260205939d4374ffd
brendanjbond <brendanbond@gmail.com>
2024-07-11 09:41:07 +0100

    conditionally check for Element

--- src/polyfills/ElementPolyfill.js
+++ src/polyfills/ElementPolyfill.js
@@ -1,12 +1,12 @@
 // Using polyfill from https://developer.mozilla.org/en-US/docs/Web/API/Element/closest
 /* eslint-disable */
 if (typeof window !== 'undefined' && typeof window.document !== 'undefined') {
-  if (!Element.prototype.matches) {
+  if (!Element?.prototype.matches) {
     Element.prototype.matches = Element.prototype.msMatchesSelector ||
       Element.prototype.webkitMatchesSelector;
   }
 
-  if (!Element.prototype.closest) {
+  if (!Element?.prototype.closest) {
     Element.prototype.closest = function(s) {
       var el = this;
       do {
