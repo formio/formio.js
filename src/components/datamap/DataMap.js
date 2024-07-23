fda351142496cafd9d9a691a1cfbb13270575ab9
ICX\Tatsiana.Hashtold <tanya@form.io>
2024-07-23 10:05:13 +0300

    FIO-8710: fixed incorrect Components import that causes errors when interacting with form components

--- src/components/datamap/DataMap.js
+++ src/components/datamap/DataMap.js
@@ -3,7 +3,7 @@ import DataGridComponent from '../datagrid/DataGrid';
 import _ from 'lodash';
 import EventEmitter from 'eventemitter3';
 import { componentValueTypes, getComponentSavedTypes, uniqueKey } from '../../utils/utils';
-import { Components } from '../Components';
+import Components from '../Components';
 
 export default class DataMapComponent extends DataGridComponent {
   static schema(...extend) {
