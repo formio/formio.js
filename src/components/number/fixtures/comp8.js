6e09d72a81fc45ef9c270ea29a670e3108c24afe
lane-formio <lane@form.io>
2024-07-24 16:32:22 -0500

    Fix linting errors

--- src/components/number/fixtures/comp8.js
+++ src/components/number/fixtures/comp8.js
@@ -1,26 +1,26 @@
 export default {
   components: [
     {
-      "label": "Number",
-      "applyMaskOn": "change",
-      "mask": false,
-      "tableView": false,
-      "delimiter": false,
-      "requireDecimal": false,
-      "inputFormat": "plain",
-      "truncateMultipleSpaces": false,
-      "key": "number",
-      "type": "number",
-      "input": true,
-      "decimalSymbol": "-"
+      label: 'Number',
+      applyMaskOn: 'change',
+      mask: false,
+      tableView: false,
+      delimiter: false,
+      requireDecimal: false,
+      inputFormat: 'plain',
+      truncateMultipleSpaces: false,
+      key: 'number',
+      type: 'number',
+      input: true,
+      decimalSymbol: '-'
     },
     {
-      "type": "button",
-      "label": "Submit",
-      "key": "submit",
-      "disableOnInvalid": true,
-      "input": true,
-      "tableView": false
+      type: 'button',
+      label: 'Submit',
+      key: 'submit',
+      disableOnInvalid: true,
+      input: true,
+      tableView: false
     }
   ]
-}
+};
