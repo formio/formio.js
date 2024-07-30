d33deaf6259c570be890d318d7acdda071d19602
ZenMasterJacob20011 <jsnichols0404@hotmail.com>
2024-07-30 16:15:33 -0500

    added config.myApiUrl property to webform.form instances

--- src/Form.js
+++ src/Form.js
@@ -3,6 +3,7 @@ import { Formio } from './Formio';
 import Displays from './displays';
 import templates from './templates';
 import * as FormioUtils from './utils/utils';
+import _ from "lodash";
 
 export default class Form extends Element {
   /**
@@ -315,6 +316,7 @@ export default class Form extends Element {
               this.instance = this.instance || this.create(form.display);
               const options = this.getFormInitOptions(formParam, form);
               this.instance.setUrl(formParam, options);
+              _.set(form, 'config.myApiUrl', options.project);
               this.instance.nosubmit = false;
               this._form = this.instance.form = form;
               if (submission) {
