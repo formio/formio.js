a8ed692aaa5f81f99c3dab880137e49b614c5110
Brendan Bond <brendanbond@users.noreply.github.com>
2024-09-10 03:08:25 -0500

    FIO-8912 update to model types (#5800)
    * update normalization to utilize component model types if applicable; fix tests
    
    * update to core dependency
    
    * add tests
    
    * minor update to tests
    
    * update deps, fix tests

--- src/components/hidden/Hidden.unit.js
+++ src/components/hidden/Hidden.unit.js
@@ -1,5 +1,8 @@
+
+
 import Harness from '../../../test/harness';
 import HiddenComponent from './Hidden';
+import assert from 'power-assert';
 
 import {
   comp1
@@ -8,5 +11,22 @@ import {
 describe('Hidden Component', () => {
   it('Should build a hidden component', () => {
     return Harness.testCreate(HiddenComponent, comp1);
+  });
+
+  it('Should not incorrectly validate multiple when hidden component has an array value', () => {
+    return Harness.testCreate(HiddenComponent, comp1).then((component) => {
+      assert(component.checkValidity(), 'Item should be valid');
+      component.setValue([
+        {
+          key: 'foo',
+          value: 'bar'
+        },
+        {
+          key: 'hello',
+          value: 'world'
+        }
+      ]);
+      assert(component.checkValidity(), 'Item should be valid after setting value');
+    });
   });
 });
