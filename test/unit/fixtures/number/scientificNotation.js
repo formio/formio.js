7c5966f7e12cd7b3aafc09d275a656cc87963e1c
Edwin Anciani <edwinanciani@gmail.com>
2024-10-28 08:42:11 -0500

    FIO-9173: Allow Scientific Notations (#5878)
    * FIO-9173: Allow Scientific Notations
    
    * FIO-9173: Fixing index
    
    * FIO-9173: Adding more tests & using parseFloat instead of Number

---
+++ test/unit/fixtures/number/scientificNotation.js
@@ -0,0 +1,13 @@
+export default {
+  'label': 'Number',
+  'mask': false,
+  'spellcheck': true,
+  'tableView': true,
+  'delimiter': true,
+  'requireDecimal': true,
+  'inputFormat': 'plain',
+  'key': 'number',
+  'type': 'number',
+  'decimalLimit': 2,
+  'input': true
+};
