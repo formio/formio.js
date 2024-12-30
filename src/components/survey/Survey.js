d07386c934e4a67c816ef14cec6e85cdceeb5412
ICX\Tatsiana.Hashtold <tanya@form.io>
2024-12-30 12:16:57 +0300

    Fixed tests

--- src/components/survey/Survey.js
+++ src/components/survey/Survey.js
@@ -151,8 +151,8 @@ export default class SurveyComponent extends Field {
         <table border="1" style="width:100%">
           <thead>
             <tr>
-              <th>${this.t('question')}</th>
-              <th>${this.t('value')}</th>
+              <th>${this.t('surveyQuestion')}</th>
+              <th>${this.t('surveyQuestionValue')}</th>
             </tr>
           </thead>
           <tbody>
