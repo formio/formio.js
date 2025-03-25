79202f252f1d3a79512b8c5f97b8ef913de0c4f0
ICX\Tatsiana.Hashtold <tanya@form.io>
2025-03-25 14:31:18 +0300

    FIO-19866: added  fileUploadingEnd and fielUplaodingStart events back

---
+++ test/forms/formWithFileComponent.js
@@ -0,0 +1,51 @@
+export const form = {
+  _id: '67e12a989ec98121f71fa67e',
+  title: 'file upload',
+  name: 'fileUpload',
+  path: 'fileupload',
+  type: 'form',
+  display: 'form',
+  owner: '637b2e6b48c1227e60b1f910',
+  components: [
+    {
+      label: 'Upload',
+      tableView: false,
+      storage: 'base64',
+      webcam: false,
+      capture: false,
+      fileTypes: [
+        {
+          label: '',
+          value: '',
+        },
+      ],
+      validateWhenHidden: false,
+      key: 'file',
+      type: 'file',
+      input: true,
+    },
+    {
+      type: 'button',
+      label: 'Submit',
+      key: 'submit',
+      disableOnInvalid: true,
+      input: true,
+      tableView: false,
+    },
+  ],
+  project: '67caad5b0416ffb92916c9ad',
+  created: '2025-03-24T09:49:12.892Z',
+  modified: '2025-03-25T08:25:20.906Z',
+  machineName: 'bcmuuifnsbrkvdx:fileUpload',
+};
+
+export const files = [
+  {
+    lastModified: 1742889250910,
+    lastModifiedDate: new Date(),
+    name: 'textFieldCalendar (1).json',
+    size: 34387,
+    type: 'application/json',
+    webkitRelativePath: '',
+  },
+];
