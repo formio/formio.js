cf540114163ea4aaa5c0f37e9b03c29ecf37cbed
Yauheni Balashka <yauheni@form.io>
2025-05-05 16:25:07 +0300

    FIO-10096: Add test

--- test/unit/TextArea.unit.js
+++ test/unit/TextArea.unit.js
@@ -7,7 +7,7 @@ import formWithRichTextAreas from '../forms/formWithRichTextAreas';
 import textAreaJsonType from '../forms/textAreaJsonType';
 import Harness from '../harness';
 import { Formio } from '../../src/Formio';
-import { comp1, comp2, comp3, comp4 } from './fixtures/textarea';
+import { comp1, comp2, comp3, comp4, comp5 } from './fixtures/textarea';
 import TextAreaComponent from '../../src/components/textarea/TextArea';
 import { fastCloneDeep } from '@formio/core';
 window.ace = require('ace-builds');
@@ -17,6 +17,18 @@ describe('TextArea Component', () => {
     return Harness.testCreate(TextAreaComponent, comp1).then((component) => {
       Harness.testElements(component, 'textarea', 1);
     });
+  });
+
+  it("Redraw in setValue should not be called if a component with multiple flag = true and renderMode = html", async function () {
+    const element = document.createElement("div");
+    const form = await Formio.createForm(element, comp5, {
+      readOnly: true,
+      renderMode: "html",
+    });
+    const component = form.getComponent("textArea");
+    const emit = sinon.spy(component, "redraw");
+    component.setValue(["Hello"]);
+    assert.equal(emit.callCount, 0);
   });
 
   it('setValue should be called only once', () => {
