048ced3967290d91a87628ad3edbabc3ba56d625
John Teague <164385719+johnformio@users.noreply.github.com>
2025-05-21 12:21:52 -0500

    Monorepo sync (#6133)
    * added sync gh action
    
    * updated sync action and script
    
    * using zx only in action
    
    * updated dependencies
    
    ---------
    
    Co-authored-by: John Teague <johnt@formio.com>

--- .github/workflows/sync-monorepo.mjs
+++ .github/workflows/sync-monorepo.mjs
@@ -1,6 +1,7 @@
-import { sync } from 'monorepo-sync';
 
-sync()
+import { syncfromGithubAction } from 'monorepo-sync';
+
+syncfromGithubAction()
 	.then(() => {
 		console.log('Sync completed successfully');
 		process.exit(0);
@@ -8,4 +9,5 @@ sync()
 	.catch((error) => {
 		console.error('Error during sync:', error);
 		process.exit(1);
+
 	});
