25c880ea31b19736e53e507ccfdad7dac6373b99
Brendan J Bond <brendanbond@gmail.com>
2025-05-22 13:24:07 -0500

    even though I hate this, I am reexporting moment and lodash

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,13 +2,17 @@ import * as utils from './utils';
 import * as formUtils from './formUtils';
 import { Evaluator, registerEvaluator, interpolate } from './Evaluator';
 import ConditionOperators from './conditionOperators';
+import _ from 'lodash';
+import moment from 'moment';
 
 const FormioUtils = {
   ...utils,
   ...formUtils,
   Evaluator,
   interpolate,
-  ConditionOperators
+  ConditionOperators,
+  _,
+  moment
 };
 if (typeof global === 'object') {
   global.FormioUtils = FormioUtils;
