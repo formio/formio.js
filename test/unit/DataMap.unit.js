8c8d284354c17245c91dc545cd54de950c3a1142
yauheni-formio <yauheni@form.io>
2025-06-13 18:23:26 +0300

    FIO-10183: Value for select is not cleared on refresh options (#6156)
    * FIO-10183: Clear select when changing data map
    
    * FIO-10183: Add test

--- test/unit/DataMap.unit.js
+++ test/unit/DataMap.unit.js
@@ -1,8 +1,9 @@
 import { Formio } from '../../src/Formio';
 import Harness from '../harness';
 import DataMapComponent from '../../src/components/datamap/DataMap';
-import { comp1, formWithConditionalPanel } from './fixtures/datamap';
+import { comp1, formWithConditionalPanel, selectClearOnRefreshForDataMap } from './fixtures/datamap';
 import assert from 'power-assert';
+import { wait } from '../util';
 
 describe('DataMap Component', () => {
   it('Should build a data map component', () => {
@@ -18,6 +19,38 @@ describe('DataMap Component', () => {
       });
     });
   });
+
+  it('Select with refreshOn=Data Map should be cleared when the Data Map fields change (clearOnRefresh should be enabled) ', async () => {
+    const dispatchNewValue = (dataMap, key) => {
+      const textFieldInput = dataMap.getComponent([key]).refs.input[0];
+      textFieldInput.value = "new value";
+      const inputEvent = new Event('input');
+      textFieldInput.dispatchEvent(inputEvent);
+    }
+    const form = await Formio.createForm(document.createElement('div'), selectClearOnRefreshForDataMap, { readOnly: true })
+    const dataMap = form.getComponent(['dataMap']);
+    const select = form.getComponent(['selectDataMap']);
+    select.setValue('a');
+    assert.equal(form.data.selectDataMap, "a");
+    dataMap.addRow();
+    await wait(500);
+    assert.equal(form.data.selectDataMap, "");
+    select.setValue('a');
+    assert.equal(form.data.selectDataMap, "a");
+    dispatchNewValue(dataMap, "__key")
+    await wait(500);
+    assert.equal(form.data.selectDataMap, "");
+    select.setValue('b');
+    assert.equal(form.data.selectDataMap, "b");
+    dispatchNewValue(dataMap, "key")
+    await wait(500);
+    assert.equal(form.data.selectDataMap, "");
+    select.setValue('c');
+    assert.equal(form.data.selectDataMap, "c");
+    dataMap.removeRow();
+    await wait(500);
+    assert.equal(form.data.selectDataMap, "");
+  })
 
   it(
     'Should render data from submission properly when the Data Map is inside conditionally shown layout component',
