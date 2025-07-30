acb06a39326dd8761cc149102ff3cafd970fe558
lane-formio <lane@form.io>
2025-07-30 18:21:19 -0500

    Fix cherry-pick to remove comp14
    comp14 was added in a cherry-pick, but the actual file doesn't exist in this branch

--- test/unit/fixtures/radio/index.js
+++ test/unit/fixtures/radio/index.js
@@ -11,6 +11,5 @@ import comp10 from './comp10';
 import comp11 from './comp11';
 import comp12 from './comp12';
 import comp13 from './comp13';
-import comp14 from './comp14';
 import comp15 from './comp15';
-export { comp1, comp2, comp3, comp4, comp5, comp6, comp7, comp8, comp9, comp10, comp11, comp12, comp13, comp14, comp15 };
+export { comp1, comp2, comp3, comp4, comp5, comp6, comp7, comp8, comp9, comp10, comp11, comp12, comp13, comp15 };
